f6015f2eb7566c44bb3b89268f0b463ae8e82b7c
trylock <drahomir.hanak@gmail.com>
2018-02-24 17:12:06 +0100

    Add attribute reader/writer factories

---
+++ Viewer/Data/Formats/IAttributeReaderFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetadataExtractor.Formats.Jpeg;
+
+namespace Viewer.Data.Formats
+{
+    public interface IAttributeReaderFactory
+    {
+        /// <summary>
+        /// Create attribute reader from JPEG segments loaded in memory
+        /// </summary>
+        /// <param name="segments">JPEG segments</param>
+        /// <returns>Attribute reader of given segments</returns>
+        IAttributeReader CreateFromSegments(IList<JpegSegment> segments);
+    }
+}
---
+++ Viewer/Data/Formats/IAttributeWriterFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Data.Formats
+{
+    public interface IAttributeWriterFactory
+    {
+        /// <summary>
+        /// Create a new attribute writer.
+        /// </summary>
+        /// <returns>New attribute writer</returns>
+        IAttributeWriter Create();
+    }
+}

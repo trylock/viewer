cc7f93a97248d1ab18daf0db925417a8d6853ed1
trylock <drahomir.hanak@gmail.com>
2018-02-24 17:04:12 +0100

    Attribute storage API is blocking

--- Viewer/Data/IAttributeStorage.cs
+++ Viewer/Data/IAttributeStorage.cs
@@ -4,26 +4,32 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Viewer.Data.Formats;
 
 namespace Viewer.Data
 {
+    /// <summary>
+    /// Facade for the attribute storage system. 
+    /// </summary>
     public interface IAttributeStorage
     {
         /// <summary>
-        /// Load attributes from given path
+        /// Load attributes from given path.
+        /// This is a blocking operation.
         /// </summary>
         /// <param name="path">Path to a file which contains some attributes</param>
         /// <exception cref="FileNotFoundException">Given file was not found</exception>
-        /// <returns>Task whose result is a collection of attributes form given file</returns>
-        Task<AttributeCollection> Load(string path);
+        /// <exception cref="InvalidDataFormatException">Attribute format is invalid</exception>
+        /// <returns>Collection of attributes found in given file</returns>
+        AttributeCollection Load(string path);
 
         /// <summary>
-        /// Store attributes to a path
+        /// Store attributes to a path.
+        /// This is a blocking operation.
         /// </summary>
         /// <param name="path">Path to a file</param>
         /// <param name="attrs">Attributes to store in this file</param>
         /// <exception cref="FileNotFoundException">Given file was not found</exception>
-        /// <returns>Task completed when the attributes are successfully written</returns>
-        Task Store(string path, AttributeCollection attrs);
+        void Store(string path, AttributeCollection attrs);
     }
 }

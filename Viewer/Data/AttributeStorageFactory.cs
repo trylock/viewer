c402035ec58634e1785e3f0d42af8643b4c99fb0
trylock <drahomir.hanak@gmail.com>
2018-02-24 17:18:46 +0100

    Add basic implementation of the FileSystemAttributeStorage

---
+++ Viewer/Data/AttributeStorageFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Viewer.Data.Formats;
+using Viewer.Data.Formats.Attributes;
+using Viewer.Data.Formats.Jpeg;
+
+namespace Viewer.Data
+{
+    public class AttributeStorageFactory
+    {
+        /// <summary>
+        /// Create attribute storage 
+        /// </summary>
+        /// <returns></returns>
+        public IAttributeStorage Create()
+        {
+            var segmentReaderFactory = new JpegSegmentReaderFactory();
+            var segmentWriterFactory = new JpegSegmentWriterFactory();
+            var attrReaderFactories = new List<IAttributeReaderFactory>
+            {
+                new AttributeReaderFactory(),
+                new ExifAttributeReaderFactory()
+            };
+            var attrWriterFactory = new AttributeWriterFactory();
+            var collectionFactory = new AttributeCollectionFactory();
+            return new FileSystemAttributeStorage(
+                segmentReaderFactory, 
+                segmentWriterFactory, 
+                collectionFactory, 
+                attrWriterFactory, 
+                attrReaderFactories);
+        }
+    }
+}
--- Viewer/Data/FileSystemAttributeStorage.cs
+++ Viewer/Data/FileSystemAttributeStorage.cs
@@ -4,21 +4,146 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
 using MetadataExtractor.Formats.Jpeg;
 using MetadataExtractor.IO;
+using Viewer.Data.Formats;
+using Viewer.Data.Formats.Attributes;
+using Viewer.Data.Formats.Jpeg;
+using Directory = MetadataExtractor.Directory;
 
 namespace Viewer.Data
 {
+    /// <summary>
+    /// Attribute storage which stores attributes directly in JPEG files.
+    /// </summary>
     public class FileSystemAttributeStorage : IAttributeStorage
     {
-        public Task<AttributeCollection> Load(string path)
+        private readonly IAttributeCollectionFactory _attrCollectionFactory;
+        private readonly IJpegSegmentReaderFactory _segmentReaderFactory;
+        private readonly IJpegSegmentWriterFactory _segmentWriterFactory;
+        private readonly IAttributeWriterFactory _attrWriterFactory;
+        private readonly IList<IAttributeReaderFactory> _attrReaderFactories;
+
+        public FileSystemAttributeStorage(
+            IJpegSegmentReaderFactory segmentReaderFactory, 
+            IJpegSegmentWriterFactory segmentWriterFactory,
+            IAttributeCollectionFactory attrCollectionFactory,
+            IAttributeWriterFactory attrWriterFactory,
+            IList<IAttributeReaderFactory> attrReaderFactories)
         {
-            throw new NotImplementedException();
+            _segmentReaderFactory = segmentReaderFactory;
+            _segmentWriterFactory = segmentWriterFactory;
+            _attrCollectionFactory = attrCollectionFactory;
+            _attrWriterFactory = attrWriterFactory;
+            _attrReaderFactories = attrReaderFactories;
         }
 
-        public Task Store(string path, AttributeCollection attrs)
+        /// <summary>
+        /// Load attributes from given file.
+        /// Read algorithm:
+        /// (1) read all JPEG segments to memory
+        /// (2) for each attribute reader in _attrReaderFactories:
+        /// (2.1) create the reader from read segments
+        /// (2.2) read all attributes and add them to the attributes collection 
+        /// </summary>
+        /// <param name="path">Path to a file</param>
+        /// <exception cref="InvalidDataFormatException">
+        ///     Format of attributes in some segment is invalid.
+        /// </exception>
+        /// <returns>Collection of attributes read from the file</returns>
+        public AttributeCollection Load(string path)
         {
-            throw new NotImplementedException();
+            // read all JPEG segments to memory
+            AttributeCollection attrs;
+            var segments = new List<JpegSegment>();
+            using (var segmentReader = _segmentReaderFactory.CreateFromPath(path))
+            {
+                attrs = _attrCollectionFactory.CreateFromPath(path);
+                for (;;)
+                {
+                    // read segment
+                    var segment = segmentReader.ReadNext();
+                    if (segment == null)
+                        break;
+
+                    // we only parse data in APP1 segments
+                    if (segment.Type != JpegSegmentType.App1)
+                        continue;
+
+                    segments.Add(segment);
+                }
+            }
+
+            // read attributes from all sources and add them to the collection
+            foreach (var factory in _attrReaderFactories)
+            {
+                var attrReader = factory.CreateFromSegments(segments);
+                for (;;)
+                {
+                    var attr = attrReader.ReadNext();
+                    if (attr == null)
+                        break;
+                    attrs.SetAttribute(attr);
+                }
+            }
+
+            return attrs;
+        }
+
+        public void Store(string path, AttributeCollection attrs)
+        {
+            string tmpFileName;
+
+            using (var segmentReader = _segmentReaderFactory.CreateFromPath(path))
+            {
+                using (var segmentWriter = _segmentWriterFactory.CreateFromPath(path, out tmpFileName))
+                {
+                    // copy all but attribute segments
+                    for (;;)
+                    {
+                        var segment = segmentReader.ReadNext();
+                        if (segment == null)
+                            break;
+
+                        if (IsAttributeSegment(segment) ||
+                            segment.Type == JpegSegmentType.Sos)
+                        {
+                            continue;
+                        }
+
+                        segmentWriter.WriteSegment(segment);
+                    }
+
+                    // write attributes
+                    var attrWriter = _attrWriterFactory.Create();
+                    foreach (var attr in attrs)
+                    {
+                        attrWriter.Write(attr);
+                    }
+
+                    // write attribute segments
+                    var segments = attrWriter.Finish();
+                    foreach (var segment in segments)
+                    {
+                        segmentWriter.WriteSegment(segment);
+                    }
+
+                    // write image data
+                    segmentWriter.Finish(segmentReader.BaseStream);
+                }
+
+                // replace the original file with the modified file
+                File.Replace(tmpFileName, path, null);
+            }
+        }
+
+        private bool IsAttributeSegment(JpegSegment segment)
+        {
+            const string header = AttributeReader.JpegSegmentHeader;
+            return segment.Type == JpegSegmentType.App1 &&
+                   Encoding.UTF8.GetString(segment.Bytes, 0, header.Length) == header;
         }
     }
 }

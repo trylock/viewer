05eda417d8d06b20c4eac661c1de7d39706b34f9
trylock <drahomir.hanak@gmail.com>
2018-02-27 19:44:24 +0100

    AttributeCollection implements ICollection
    The name would be misleading otherwise.

--- Viewer/Data/AttributeCollection.cs
+++ Viewer/Data/AttributeCollection.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Viewer.Data
 {
-    public class AttributeCollection : IEnumerable<Attribute>
+    public class AttributeCollection : ICollection<Attribute>
     {
         private Dictionary<string, Attribute> _attrs = new Dictionary<string, Attribute>();
 
@@ -32,12 +32,30 @@ namespace Viewer.Data
         ///          them back to a file
         /// </summary>
         public bool IsDirty { get; private set; }
+        
+        /// <summary>
+        /// Number or attributes in the collection
+        /// </summary>
+        public int Count => _attrs.Count;
+
+        /// <summary>
+        /// false, the collection is never read-only
+        /// </summary>
+        public bool IsReadOnly => false;
 
         public AttributeCollection(string path, DateTime lastWriteTime, DateTime lastAccessTime)
         {
             Path = path;
             LastWriteTime = lastWriteTime;
             LastAccessTime = lastAccessTime;
+        }
+
+        public AttributeCollection(string path) 
+        {
+            var fi = new FileInfo(path);
+            Path = path;
+            LastWriteTime = fi.LastWriteTime;
+            LastAccessTime = fi.LastAccessTime;
         }
 
         /// <summary>
@@ -79,6 +97,70 @@ namespace Viewer.Data
             {
                 _attrs.Add(attr.Name, attr);
             }
+        }
+        
+        /// <summary>
+        /// Add a new attribute or set an existing attribute
+        /// </summary>
+        /// <param name="attr"></param>
+        public void Add(Attribute attr)
+        {
+            SetAttribute(attr);
+        }
+
+        /// <summary>
+        /// Remove all attributes in the collection
+        /// </summary>
+        public void Clear()
+        {
+            _attrs.Clear();
+            IsDirty = true;
+        }
+
+        /// <summary>
+        /// Check whether there is an attribute
+        /// </summary>
+        /// <param name="attr"></param>
+        /// <returns>true iff given attribute is in the collection</returns>
+        public bool Contains(Attribute attr)
+        {
+            if (!_attrs.TryGetValue(attr.Name, out Attribute attrInCollection))
+            {
+                return false;
+            }
+
+            return attr == attrInCollection;
+        }
+
+        /// <summary>
+        /// Copy the attributes to an array
+        /// </summary>
+        /// <param name="array">Array of attributes where the attributes will be copied</param>
+        /// <param name="arrayIndex">Index in the array at which copying begins</param>
+        public void CopyTo(Attribute[] array, int arrayIndex)
+        {
+            _attrs.Values.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Remove attribute with the same name and value
+        /// </summary>
+        /// <param name="item">Attribute</param>
+        /// <returns>true iff the attribute has been removed</returns>
+        public bool Remove(Attribute item)
+        {
+            if (!_attrs.TryGetValue(item.Name, out Attribute attrInCollection))
+            {
+                return false;
+            }
+
+            if (item != attrInCollection)
+            {
+                return false;
+            }
+
+            _attrs.Remove(item.Name);
+            return true;
         }
 
         public IEnumerator<Attribute> GetEnumerator()

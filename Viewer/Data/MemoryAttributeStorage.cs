4754f5d1b8561d9e4cfd982458283c68693c4af7
trylock <drahomir.hanak@gmail.com>
2018-03-03 12:28:57 +0100

    Add memory attribute storage
    This is mainly for testing.

---
+++ Viewer/Data/MemoryAttributeStorage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Data
+{
+    public class MemoryAttributeStorage : IAttributeStorage
+    {
+        private Dictionary<string, AttributeCollection> _files = new Dictionary<string, AttributeCollection>();
+
+        public IEnumerable<AttributeCollection> Files
+        {
+            get
+            {
+                foreach (var pair in _files)
+                {
+                    yield return pair.Value;
+                }
+            }
+        }
+
+        public AttributeCollection Load(string path)
+        {
+            if (!_files.TryGetValue(path, out AttributeCollection collection))
+            {
+                return new AttributeCollection(path, DateTime.Now, DateTime.Now);
+            }
+
+            return collection;
+        }
+
+        public void Store(string path, AttributeCollection attrs)
+        {
+            if (_files.ContainsKey(path))
+            {
+                _files[path] = attrs;
+            }
+            else
+            {
+                _files.Add(path, attrs);
+            }
+        }
+    }
+}

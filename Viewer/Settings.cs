6c2de46c66b45d765be18fdc311b2a1ded034db9
trylock <drahomir.hanak@gmail.com>
2018-03-03 12:26:45 +0100

    Add Settings class
    The class manages application settings. It should be loaded from file.

---
+++ Viewer/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using MetadataExtractor.Formats.Exif;
+using Viewer.Data.Formats;
+using Viewer.Data.Formats.Attributes;
+using Viewer.Data.Formats.Exif;
+
+namespace Viewer
+{
+    public class Settings
+    {
+        public static Settings Instance = new Settings();
+
+        public IList<IExifAttributeParser> ExifTags { get; }
+
+        public Settings()
+        {
+            ExifTags = new List<IExifAttributeParser>
+            {
+                new Ifd0ExifAttributeParser("ImageWidth", ExifIfd0Directory.TagImageWidth, AttributeType.Int),
+                new Ifd0ExifAttributeParser("ImageHeight", ExifIfd0Directory.TagImageHeight, AttributeType.Int),
+                new Ifd0ExifAttributeParser("Model", ExifIfd0Directory.TagModel, AttributeType.String),
+                new Ifd0ExifAttributeParser("Make", ExifIfd0Directory.TagMake, AttributeType.String),
+                new ThumbnaiExifAttributeParser()
+            };
+        }
+    }
+}

4484fe22ed626b44840dd763a309008dfbbbf9fc
trylock <drahomir.hanak@gmail.com>
2018-03-10 20:02:02 +0100

    AttributeWriter factory creates the writer from output stream
    The attributes are now written to the stream.

--- Viewer/Data/Formats/IAttributeWriterFactory.cs
+++ Viewer/Data/Formats/IAttributeWriterFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,12 @@ namespace Viewer.Data.Formats
     public interface IAttributeWriterFactory
     {
         /// <summary>
-        /// Create a new attribute writer.
+        /// Create attribute writer
         /// </summary>
+        /// <param name="output">
+        ///     Output stream where the writer will store serialized attribute data
+        /// </param>
         /// <returns>New attribute writer</returns>
-        IAttributeWriter Create();
+        IAttributeWriter Create(Stream output);
     }
 }

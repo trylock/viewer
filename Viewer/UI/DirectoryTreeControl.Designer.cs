fc10405c0f51ffb0c932784059ed1b8eb69653f1
trylock <lothar.gordon@gmail.com>
2018-03-15 13:13:28 +0100

    Add basic DirectoryTreeControl

---
+++ Viewer/UI/DirectoryTreeControl.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Viewer.UI
+{
+    partial class DirectoryTreeControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TreeView = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // TreeView
+            // 
+            this.TreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TreeView.Location = new System.Drawing.Point(0, 0);
+            this.TreeView.Name = "TreeView";
+            this.TreeView.Size = new System.Drawing.Size(457, 497);
+            this.TreeView.TabIndex = 0;
+            this.TreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.TreeView_NodeMouseClick);
+            // 
+            // DirectoryTreeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TreeView);
+            this.Name = "DirectoryTreeControl";
+            this.Size = new System.Drawing.Size(457, 497);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TreeView;
+    }
+}
---
+++ Viewer/UI/DirectoryTreeControl.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Viewer.Properties;
+
+namespace Viewer.UI
+{
+    public partial class DirectoryTreeControl : UserControl
+    {
+        /// <summary>
+        /// Directory with at least one of these flags will be hidden.
+        /// </summary>
+        public FileAttributes HideFlags = FileAttributes.Hidden;
+
+        public DirectoryTreeControl()
+        {
+            InitializeComponent();
+
+            // initialize the tree view with ready logical drives
+            TreeView.BeginUpdate();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (!drive.IsReady)
+                    continue;
+
+                TreeView.Nodes.Add(drive.Name, drive.Name);
+            }
+            TreeView.EndUpdate();
+        }
+
+        private void TreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.IsExpanded)
+            {
+                e.Node.Collapse();
+            }
+            else
+            {
+                UpdateSubdirectories(e.Node);
+            }
+        }
+
+        private void UpdateSubdirectories(TreeNode node)
+        {
+            var di = new DirectoryInfo(node.FullPath);
+
+            TreeView.BeginUpdate();
+
+            // remove old subdirectories
+            node.Nodes.Clear();
+
+            // add new subdirectories
+            try
+            {
+                foreach (var directory in di.EnumerateDirectories())
+                {
+                    if ((directory.Attributes & HideFlags) != 0)
+                        continue;
+
+                    node.Nodes.Add(directory.FullName, directory.Name);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show(
+                    string.Format(Resources.UnauthorizedAccess_Message, di.Name), 
+                    Resources.UnauthorizedAccess_Label, 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Warning);
+            }
+
+            // make sure the node is expanded
+            node.Expand();
+            TreeView.EndUpdate();
+        }
+    }
+}

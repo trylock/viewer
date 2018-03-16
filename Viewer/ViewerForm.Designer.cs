53316121560c7252063832849d61798c8c73ecfe
trylock <lothar.gordon@gmail.com>
2018-03-16 21:09:36 +0100

    Add basic structure of the main form

--- Viewer/ViewerForm.Designer.cs
+++ Viewer/ViewerForm.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.directoryTreeControl1 = new Viewer.UI.DirectoryTreeControl();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SplitContainer
+            // 
+            this.SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.SplitContainer.Name = "SplitContainer";
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.Controls.Add(this.directoryTreeControl1);
+            this.SplitContainer.Size = new System.Drawing.Size(967, 593);
+            this.SplitContainer.SplitterDistance = 289;
+            this.SplitContainer.TabIndex = 0;
+            // 
+            // directoryTreeControl1
+            // 
+            this.directoryTreeControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.directoryTreeControl1.Location = new System.Drawing.Point(0, 0);
+            this.directoryTreeControl1.Name = "directoryTreeControl1";
+            this.directoryTreeControl1.Size = new System.Drawing.Size(289, 593);
+            this.directoryTreeControl1.TabIndex = 0;
+            // 
+            // ViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(967, 593);
+            this.Controls.Add(this.SplitContainer);
+            this.Name = "ViewerForm";
+            this.Text = "Viewer";
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private UI.DirectoryTreeControl directoryTreeControl1;
     }
 }
 

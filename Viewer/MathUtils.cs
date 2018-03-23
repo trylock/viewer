d72c087b0387af359d0ac9f5afb890adcb2767b6
trylock <lothar.gordon@gmail.com>
2018-03-23 19:59:45 +0100

    Add RoundUpDiv
    Utility method to compute integer divison and round the result up.
    Only works for non-negative integers.

---
+++ Viewer/MathUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer
+{
+    public static class MathUtils
+    {
+        /// <summary>
+        /// Divide <paramref name="num"/> with <paramref name="denom"/> and round the result up.
+        /// I.e.: compute "ceil(num / denom)"
+        /// </summary>
+        /// <param name="num">Numerator</param>
+        /// <param name="denom">Denominator</param>
+        /// <returns>Result of the division rounded up</returns>
+        public static int RoundUpDiv(int num, int denom)
+        {
+            if (num < 0)
+                throw new ArgumentOutOfRangeException(nameof(num));
+            if (denom < 0)
+                throw new ArgumentOutOfRangeException(nameof(denom));
+            return (num + denom - 1) / denom;
+        }
+    }
+}

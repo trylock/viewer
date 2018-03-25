151516417b83c79fd7f0e23b4bdeace169d00a81
trylock <lothar.gordon@gmail.com>
2018-03-25 23:23:06 +0200

    Add PathUtils
    Utility functions for parsing file system paths

---
+++ Viewer/IO/PathUtils.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.IO
+{
+    public static class PathUtils
+    {
+        /// <summary>
+        /// List of characters that could be used as a path separators
+        /// </summary>
+        public static char[] PathSeparators =
+        {
+            Path.DirectorySeparatorChar,
+            Path.AltDirectorySeparatorChar
+        };
+
+        /// <summary>
+        /// Split filesystem path to parts (file and directory names)
+        /// </summary>
+        /// <param name="fullPath">Path to a directory/file</param>
+        /// <returns>Parts of the path</returns>
+        public static IEnumerable<string> Split(string fullPath)
+        {
+            return fullPath.Split(PathSeparators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Get last part of given path (i.e. file/folder name).
+        /// Note: it will return an empty string if the path ends with a path separator
+        /// </summary>
+        /// <param name="path">Path to a file or folder</param>
+        /// <returns>Last part of the path</returns>
+        public static string GetLastPart(string path)
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+
+            var lastPartLength = 0; 
+            var separatorIndex = path.Length - 1;
+            while (separatorIndex >= 0)
+            {
+                // check if there is a path separator at this position
+                var isSeparator = false;
+                foreach (var sep in PathSeparators)
+                {
+                    if (sep == path[separatorIndex])
+                    {
+                        isSeparator = true;
+                        break;
+                    }
+                }
+
+                if (isSeparator && separatorIndex + 1 != path.Length)
+                {
+                    break;
+                }
+                else if (!isSeparator)
+                {
+                    ++lastPartLength;
+                }
+
+                --separatorIndex;
+            }
+            
+            return path.Substring(separatorIndex + 1, lastPartLength);
+        }
+
+        /// <summary>
+        /// Check whether given string could be a valid file/folder name
+        /// </summary>
+        /// <param name="name">Name of a file</param>
+        /// <returns>true iff given value could be a valid file/folder name</returns>
+        public static bool IsValidFileName(string name)
+        {
+            return !String.IsNullOrEmpty(name) &&
+                   name.IndexOfAny(Path.GetInvalidFileNameChars()) < 0;
+        }
+    }
+}
---
+++ ViewerTest/IO/PathUtilsTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Viewer.IO;
+
+namespace ViewerTest.IO
+{
+    [TestClass]
+    public class PathUtilsTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetLastPart_NullPath()
+        {
+            PathUtils.GetLastPart(null);
+        }
+
+        [TestMethod]
+        public void GetLastPart_EmptyPath()
+        {
+            Assert.AreEqual("", PathUtils.GetLastPart(""));
+        }
+
+        [TestMethod]
+        public void GetLastPart_DirectoryWithPathSeparatorAtTheEnd()
+        {
+            Assert.AreEqual("C:", PathUtils.GetLastPart("C:" + Path.DirectorySeparatorChar));
+            Assert.AreEqual("lorem", PathUtils.GetLastPart(Path.Combine("C:/", "lorem") + Path.AltDirectorySeparatorChar));
+            Assert.AreEqual("..", PathUtils.GetLastPart(Path.Combine("C:/", "ipsum", "..") + Path.AltDirectorySeparatorChar));
+            Assert.AreEqual(".", PathUtils.GetLastPart(Path.Combine("C:/", "dolor", ".") + Path.DirectorySeparatorChar));
+        }
+
+        [TestMethod]
+        public void GetLastPart_DirectoryWithoutPathSeparatorAtTheEnd()
+        {
+            Assert.AreEqual("C:", PathUtils.GetLastPart("C:"));
+            Assert.AreEqual("lorem", PathUtils.GetLastPart(Path.Combine("C:/", "lorem")));
+            Assert.AreEqual("..", PathUtils.GetLastPart(Path.Combine("C:/", "ipsum", "..")));
+            Assert.AreEqual(".", PathUtils.GetLastPart(Path.Combine("C:/", "dolor", ".")));
+        }
+    }
+}

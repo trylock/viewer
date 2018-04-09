fbb3895d3f80ce6aa59e97b4583488074da4a0f8
trylock <lothar.gordon@gmail.com>
2018-04-09 12:40:39 +0200

    Move AttributeVisitor to the Attribute source

--- Viewer/Data/Attribute.cs
+++ Viewer/Data/Attribute.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 
 namespace Viewer.Data
 {
+    public interface IAttributeVisitor
+    {
+        void Visit(IntAttribute attr);
+        void Visit(DoubleAttribute attr);
+        void Visit(StringAttribute attr);
+        void Visit(DateTimeAttribute attr);
+        void Visit(ImageAttribute attr);
+    }
+
+    public interface IAttributeVisitor<out T>
+    {
+        T Visit(IntAttribute attr);
+        T Visit(DoubleAttribute attr);
+        T Visit(StringAttribute attr);
+        T Visit(DateTimeAttribute attr);
+        T Visit(ImageAttribute attr);
+    }
+
     [Flags]
     public enum AttributeFlags
     {

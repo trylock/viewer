3ce27a8d3d32d264ae26e205d7bf584f433e0c4e
trylock <lothar.gordon@gmail.com>
2018-04-14 11:45:22 +0200

    Add EntityRepository
    EntityRepository is a class that manages modified entities.

---
+++ Viewer/Data/EntityRepository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Data
+{
+    /// <summary>
+    /// EntityRepository is a thread safe collection of modified entities.
+    /// </summary>
+    public interface IEntityRepository
+    {
+        /// <summary>
+        /// Add an entity to the repository.
+        /// </summary>
+        /// <param name="entity">New entity</param>
+        void Add(IEntity entity);
+
+        /// <summary>
+        /// Try to add an entity to the repository. 
+        /// If there is an entity with the same path already, <paramref name="entity"/> won't be added.
+        /// </summary>
+        /// <param name="entity">New entity</param>
+        /// <returns>true iff <paramref name="entity"/> has been added</returns>
+        bool TryAdd(IEntity entity);
+
+        /// <summary>
+        /// Remove entity with <paramref name="path"/> from the repository
+        /// </summary>
+        /// <param name="path">Path to an entity</param>
+        void Remove(string path);
+
+        /// <summary>
+        /// Move entity from <paramref name="oldPath"/> to <paramref name="newPath"/>
+        /// </summary>
+        /// <param name="oldPath"></param>
+        /// <param name="newPath"></param>
+        void Move(string oldPath, string newPath);
+
+        /// <summary>
+        /// Remove all entities.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Move all entities currently in the repository to a snapshot and return it.
+        /// </summary>
+        /// <returns></returns>
+        IReadOnlyList<IEntity> GetSnapshot();
+    }
+    
+    public class EntityRepository : IEntityRepository
+    {
+        private Dictionary<string, IEntity> _modified = new Dictionary<string, IEntity>();
+        
+        public virtual void Add(IEntity entity)
+        {
+            lock (_modified)
+            {
+                _modified[entity.Path] = entity;
+            }
+        }
+
+        public bool TryAdd(IEntity entity)
+        {
+            lock (_modified)
+            {
+                if (_modified.ContainsKey(entity.Path))
+                {
+                    return false;
+                }
+                _modified.Add(entity.Path, entity);
+                return true;
+            }
+        }
+
+        public virtual void Remove(string path)
+        {
+            lock (_modified)
+            {
+                _modified.Remove(path);
+            }
+        }
+
+        public virtual void Move(string oldPath, string newPath)
+        {
+            lock (_modified)
+            {
+                if (!_modified.TryGetValue(oldPath, out IEntity value))
+                {
+                    return;
+                }
+
+                _modified.Remove(oldPath);
+                _modified[newPath] = value.ChangePath(newPath);
+            }
+        }
+
+        public void Clear()
+        {
+            lock (_modified)
+            {
+                _modified.Clear();
+            }
+        }
+
+        public IReadOnlyList<IEntity> GetSnapshot()
+        {
+            lock (_modified)
+            {
+                var values = _modified.Values.ToList();
+                _modified.Clear();
+                return values;
+            }
+        }
+    }
+}
---
+++ ViewerTest/Data/EntityRepositoryTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Viewer.Data;
+
+namespace ViewerTest.Data
+{
+    [TestClass]
+    public class EntityRepositoryTest
+    {
+        [TestMethod]
+        public void Add_ReplaceEntity()
+        {
+            var entity1 = new Entity("test");
+            var entity2 = new Entity("test");
+            var entity3 = new Entity("test2");
+            var rep = new EntityRepository();
+            rep.Add(entity1);
+            rep.Add(entity2);
+            rep.Add(entity3);
+
+            var data = rep.GetSnapshot().OrderBy(entity => entity.Path).ToList();
+            CollectionAssert.AreEqual(new[]{ entity2, entity3 }, data);
+        }
+
+        [TestMethod]
+        public void TryAdd_DontReplaceEntity()
+        {
+            var entity1 = new Entity("test");
+            var entity2 = new Entity("test");
+            var entity3 = new Entity("test2");
+            var rep = new EntityRepository();
+            Assert.IsTrue(rep.TryAdd(entity1));
+            Assert.IsFalse(rep.TryAdd(entity2));
+            Assert.IsTrue(rep.TryAdd(entity3));
+
+            var data = rep.GetSnapshot().OrderBy(entity => entity.Path).ToList();
+            CollectionAssert.AreEqual(new[] { entity1, entity3 }, data);
+        }
+
+        [TestMethod]
+        public void Remove_NonExistentEntity()
+        {
+            var entity1 = new Entity("test");
+            var rep = new EntityRepository();
+            rep.Add(entity1);
+            rep.Remove("test2");
+
+            var data = rep.GetSnapshot().OrderBy(entity => entity.Path).ToList();
+            CollectionAssert.AreEqual(new[] { entity1 }, data);
+        }
+
+        [TestMethod]
+        public void Move_NonExistentEntity()
+        {
+            var entity1 = new Entity("test");
+            var rep = new EntityRepository();
+            rep.Add(entity1);
+            rep.Move("test2", "test");
+
+            var data = rep.GetSnapshot().OrderBy(entity => entity.Path).ToList();
+            CollectionAssert.AreEqual(new[] { entity1 }, data);
+        }
+
+        [TestMethod]
+        public void Move_ExistingEntity()
+        {
+            var entity1 = new Entity("test");
+            var rep = new EntityRepository();
+            rep.Add(entity1);
+            rep.Move("test", "test2");
+
+            var data = rep.GetSnapshot();
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual("test2", data[0].Path);
+        }
+
+        [TestMethod]
+        public void Move_ReplaceNewPath()
+        {
+            var entity1 = new Entity("test");
+            var entity2 = new Entity("test2");
+            var rep = new EntityRepository();
+            rep.Add(entity1);
+            rep.Add(entity2);
+            rep.Move("test", "test2");
+
+            var data = rep.GetSnapshot();
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual("test2", data[0].Path);
+            Assert.AreNotEqual(entity2, data[0]);
+        }
+    }
+}

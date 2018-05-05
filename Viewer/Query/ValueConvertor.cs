3c5202d26229a9bd316a5b8d73ad6391279fba44
trylock <lothar.gordon@gmail.com>
2018-05-05 12:56:57 +0200

    Add ValueConverter
    Value converter can convert values in attributes and query expressions.

---
+++ Viewer/Query/ValueConvertor.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Viewer.Data;
+
+namespace Viewer.Query
+{
+    public interface IValueConverter
+    {
+        /// <summary>
+        /// Compute cost of convertion from <paramref name="from"/> to <paramref name="to"/>.
+        /// The cost will be int.MaxValue if the conversion is not possible
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns>Cost of the conversion</returns>
+        int ComputeConversionCost(TypeId from, TypeId to);
+
+        /// <summary>
+        /// Compute cost of conversion from types <paramref name="from"/> to types <paramref name="to"/>
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        int ComputeConversionCost(IReadOnlyList<TypeId> from, IReadOnlyList<TypeId> to);
+
+        /// <summary>
+        /// Convert <paramref name="value"/> to type <paramref name="to"/>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="to"></param>
+        /// <returns>Converted value. It won't ever be null but its value may be null</returns>
+        BaseValue ConvertTo(BaseValue value, TypeId to);
+    }
+
+    [Export(typeof(IValueConverter))]
+    public class ValueConvertor : IValueConverter
+    {
+        private class ConversionVisitor : IValueVisitor<BaseValue>
+        {
+            private readonly TypeId _resultType;
+
+            public ConversionVisitor(TypeId resultType)
+            {
+                if (resultType == TypeId.None)
+                    throw new ArgumentOutOfRangeException(nameof(resultType));
+                _resultType = resultType;
+            }
+
+            public BaseValue Visit(IntValue value)
+            {
+                switch (_resultType)
+                {
+                    case TypeId.Integer:
+                        return value;
+                    case TypeId.Real:
+                        return new RealValue(value.Value);
+                    case TypeId.String:
+                        return new StringValue(value.Value == null ? null : value.ToString());
+                    case TypeId.DateTime:
+                        return new DateTimeValue(null);
+                    case TypeId.Image:
+                        return new ImageValue(null);
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            public BaseValue Visit(RealValue value)
+            {
+                switch (_resultType)
+                {
+                    case TypeId.Integer:
+                        return new IntValue(null);
+                    case TypeId.Real:
+                        return value;
+                    case TypeId.String:
+                        return new StringValue(value.ToString());
+                    case TypeId.DateTime:
+                        return new DateTimeValue(null);
+                    case TypeId.Image:
+                        return new ImageValue(null);
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            public BaseValue Visit(StringValue value)
+            {
+                switch (_resultType)
+                {
+                    case TypeId.Integer:
+                        return new IntValue(null);
+                    case TypeId.Real:
+                        return new RealValue(null);
+                    case TypeId.String:
+                        return value;
+                    case TypeId.DateTime:
+                        if (DateTime.TryParse(value.Value, out DateTime dateValue))
+                        {
+                            return new DateTimeValue(dateValue);
+                        }
+                        return new DateTimeValue(null);
+                    case TypeId.Image:
+                        return new ImageValue(null);
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            public BaseValue Visit(DateTimeValue value)
+            {
+                switch (_resultType)
+                {
+                    case TypeId.Integer:
+                        return new IntValue(null);
+                    case TypeId.Real:
+                        return new RealValue(null);
+                    case TypeId.String:
+                        return new StringValue(value.ToString());
+                    case TypeId.DateTime:
+                        return value;
+                    case TypeId.Image:
+                        return new ImageValue(null);
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            public BaseValue Visit(ImageValue value)
+            {
+                switch (_resultType)
+                {
+                    case TypeId.Integer:
+                        return new IntValue(null);
+                    case TypeId.Real:
+                        return new RealValue(null);
+                    case TypeId.String:
+                        return new StringValue(null);
+                    case TypeId.DateTime:
+                        return new DateTimeValue(null);
+                    case TypeId.Image:
+                        return value;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        
+        public int ComputeConversionCost(TypeId from, TypeId to)
+        {
+            if (from == to)
+            {
+                return 0;
+            }
+
+            switch (from)
+            {
+                case TypeId.Integer:
+                    if (to == TypeId.Real)
+                    {
+                        return 1;
+                    }
+                    else if (to == TypeId.String)
+                    {
+                        return 2;
+                    }
+
+                    break;
+                case TypeId.Real:
+                    if (to == TypeId.String)
+                    {
+                        return 1;
+                    }
+                    break;
+                case TypeId.String:
+                    if (to == TypeId.DateTime)
+                    {
+                        return 1;
+                    }
+                    break;
+                case TypeId.DateTime:
+                    if (to == TypeId.String)
+                    {
+                        return 1;
+                    }
+                    break;
+            }
+            return int.MaxValue;
+        }
+
+        public int ComputeConversionCost(IReadOnlyList<TypeId> from, IReadOnlyList<TypeId> to)
+        {
+            if (from.Count != to.Count)
+            {
+                return int.MaxValue;
+            }
+
+            var totalCost = 0;
+            for (var i = 0; i < from.Count; ++i)
+            {
+                var cost = ComputeConversionCost(from[i], to[i]);
+                if (cost == int.MaxValue)
+                {
+                    return int.MaxValue;
+                }
+
+                totalCost += cost;
+            }
+
+            return totalCost;
+        }
+
+        public BaseValue ConvertTo(BaseValue value, TypeId to)
+        {
+            var convertor = new ConversionVisitor(to);
+            return value.Accept(convertor);
+        }
+    }
+}
---
+++ ViewerTest/Query/ValueConverterTest.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Viewer.Data;
+using Viewer.Query;
+
+namespace ViewerTest.Query
+{
+    [TestClass]
+    public class ValueConverterTest
+    {
+        private ValueConvertor _converter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _converter = new ValueConvertor();
+        }
+
+        [TestMethod]
+        public void ConvertTo_FromNullValueToIntValue()
+        {
+            var value = _converter.ConvertTo(new IntValue(null), TypeId.Integer);
+            Assert.IsNull((value as IntValue).Value);
+
+            value = _converter.ConvertTo(new RealValue(null), TypeId.Integer);
+            Assert.IsNull((value as IntValue).Value);
+
+            value = _converter.ConvertTo(new StringValue(null), TypeId.Integer);
+            Assert.IsNull((value as IntValue).Value);
+
+            value = _converter.ConvertTo(new DateTimeValue(null), TypeId.Integer);
+            Assert.IsNull((value as IntValue).Value);
+        }
+
+        [TestMethod]
+        public void ConvertTo_FromNullValueToRealValue()
+        {
+            var value = _converter.ConvertTo(new IntValue(null), TypeId.Real);
+            Assert.IsNull((value as RealValue).Value);
+
+            value = _converter.ConvertTo(new RealValue(null), TypeId.Real);
+            Assert.IsNull((value as RealValue).Value);
+
+            value = _converter.ConvertTo(new StringValue(null), TypeId.Real);
+            Assert.IsNull((value as RealValue).Value);
+
+            value = _converter.ConvertTo(new DateTimeValue(null), TypeId.Real);
+            Assert.IsNull((value as RealValue).Value);
+        }
+
+        [TestMethod]
+        public void ConvertTo_Int()
+        {
+            var value = _converter.ConvertTo(new IntValue(42), TypeId.Integer);
+            Assert.AreEqual(42, (value as IntValue).Value);
+
+            value = _converter.ConvertTo(new RealValue(42.0), TypeId.Integer);
+            Assert.IsNull((value as IntValue).Value);
+
+            value = _converter.ConvertTo(new StringValue("42"), TypeId.Integer);
+            Assert.IsNull((value as IntValue).Value);
+
+            value = _converter.ConvertTo(new DateTimeValue(DateTime.Today), TypeId.Integer);
+            Assert.IsNull((value as IntValue).Value);
+        }
+
+        [TestMethod]
+        public void ConvertTo_Real()
+        {
+            var value = _converter.ConvertTo(new IntValue(42), TypeId.Real);
+            Assert.AreEqual(42.0, (value as RealValue).Value);
+
+            value = _converter.ConvertTo(new RealValue(42.0), TypeId.Real);
+            Assert.AreEqual(42.0, (value as RealValue).Value);
+
+            value = _converter.ConvertTo(new StringValue("42.0"), TypeId.Real);
+            Assert.IsNull((value as RealValue).Value);
+
+            value = _converter.ConvertTo(new DateTimeValue(DateTime.Today), TypeId.Real);
+            Assert.IsNull((value as RealValue).Value);
+        }
+
+        [TestMethod]
+        public void ConvertTo_String()
+        {
+            var value = _converter.ConvertTo(new IntValue(42), TypeId.String);
+            Assert.AreEqual("42", (value as StringValue).Value);
+
+            value = _converter.ConvertTo(new RealValue(42.0), TypeId.String);
+            Assert.AreEqual((42.0).ToString(), (value as StringValue).Value);
+
+            value = _converter.ConvertTo(new StringValue("42.0"), TypeId.String);
+            Assert.AreEqual("42.0", (value as StringValue).Value);
+
+            var dateTime = DateTime.Now;
+            value = _converter.ConvertTo(new DateTimeValue(dateTime), TypeId.String);
+            Assert.AreEqual(dateTime.ToString(), (value as StringValue).Value);
+        }
+
+        [TestMethod]
+        public void ConvertTo_DateTime()
+        {
+            var value = _converter.ConvertTo(new IntValue(42), TypeId.DateTime);
+            Assert.IsNull((value as DateTimeValue).Value);
+
+            value = _converter.ConvertTo(new RealValue(42.0), TypeId.DateTime);
+            Assert.IsNull((value as DateTimeValue).Value);
+
+            value = _converter.ConvertTo(new StringValue("42.0"), TypeId.DateTime);
+            Assert.IsNull((value as DateTimeValue).Value);
+
+            var dateString = "04/05/2018 16:19";
+            value = _converter.ConvertTo(new StringValue(dateString), TypeId.DateTime);
+            Assert.AreEqual(DateTime.Parse(dateString), (value as DateTimeValue).Value);
+
+            var dateTime = DateTime.Now;
+            value = _converter.ConvertTo(new DateTimeValue(dateTime), TypeId.DateTime);
+            Assert.AreEqual(dateTime, (value as DateTimeValue).Value);
+        }
+        
+        [TestMethod]
+        public void ComputeConversionCost_FromInt()
+        {
+            Assert.AreEqual(0, _converter.ComputeConversionCost(TypeId.Integer, TypeId.Integer));
+            Assert.AreEqual(1, _converter.ComputeConversionCost(TypeId.Integer, TypeId.Real));
+            Assert.AreEqual(2, _converter.ComputeConversionCost(TypeId.Integer, TypeId.String));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Integer, TypeId.DateTime));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Integer, TypeId.Image));
+        }
+
+        [TestMethod]
+        public void ComputeConversionCost_FromReal()
+        {
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Real, TypeId.Integer));
+            Assert.AreEqual(0, _converter.ComputeConversionCost(TypeId.Real, TypeId.Real));
+            Assert.AreEqual(1, _converter.ComputeConversionCost(TypeId.Real, TypeId.String));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Real, TypeId.DateTime));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Real, TypeId.Image));
+        }
+
+        [TestMethod]
+        public void ComputeConversionCost_FromString()
+        {
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.String, TypeId.Integer));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.String, TypeId.Real));
+            Assert.AreEqual(0, _converter.ComputeConversionCost(TypeId.String, TypeId.String));
+            Assert.AreEqual(1, _converter.ComputeConversionCost(TypeId.String, TypeId.DateTime));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.String, TypeId.Image));
+        }
+
+        [TestMethod]
+        public void ComputeConversionCost_FromDateTime()
+        {
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.DateTime, TypeId.Integer));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.DateTime, TypeId.Real));
+            Assert.AreEqual(1, _converter.ComputeConversionCost(TypeId.DateTime, TypeId.String));
+            Assert.AreEqual(0, _converter.ComputeConversionCost(TypeId.DateTime, TypeId.DateTime));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.DateTime, TypeId.Image));
+        }
+
+        [TestMethod]
+        public void ComputeConversionCost_FromImage()
+        {
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Image, TypeId.Integer));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Image, TypeId.Real));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Image, TypeId.String));
+            Assert.AreEqual(int.MaxValue, _converter.ComputeConversionCost(TypeId.Image, TypeId.DateTime));
+            Assert.AreEqual(0, _converter.ComputeConversionCost(TypeId.Image, TypeId.Image));
+        }
+    }
+}

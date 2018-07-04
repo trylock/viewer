f5727ebfc453a1eb6754820553bb5787deb6f86c
trylock <lothar.gordon@gmail.com>
2018-07-04 15:22:42 +0200

    SortedList: add merge method
    It is now possible to merge SortedList with another sorted
    collection. The merge is a linear time operation unlike the Add
    method which is quadratic.

--- Viewer/UI/Images/SortedList.cs
+++ Viewer/UI/Images/SortedList.cs
@@ -37,6 +37,12 @@ namespace Viewer.UI.Images
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Add a new item to the list.
+        /// The list will remain sorted.
+        /// Time complexity of this operation is linear with the size of this collection.
+        /// </summary>
+        /// <param name="item"></param>
         public void Add(T item)
         {
             _values.Add(item);
@@ -57,6 +63,54 @@ namespace Viewer.UI.Images
                     break;
                 }
             }
+        }
+
+        /// <summary>
+        /// Merge 2 sorted collections to a new SortedList.
+        /// Neither collection will be modified.
+        /// Time complexity of this operation is linear with the combined size of the 2 collections.
+        /// This method does not check whether the second collection is sorted.
+        /// </summary>
+        /// <param name="other">Other sorted collection which will be merged with this list.</param>
+        /// <returns>A new sorted list which contains all elements from both lists.</returns>
+        public SortedList<T> Merge(IEnumerable<T> other)
+        {
+            var result = new SortedList<T>(Comparer);
+            using (var left = GetEnumerator())
+            using (var right = other.GetEnumerator())
+            {
+                var leftHasNext = left.MoveNext();
+                var rightHasNext = right.MoveNext();
+                while (leftHasNext || rightHasNext)
+                {
+                    if (!leftHasNext)
+                    {
+                        result._values.Add(right.Current);
+                        rightHasNext = right.MoveNext();
+                    }
+                    else if (!rightHasNext)
+                    {
+                        result._values.Add(left.Current);
+                        leftHasNext = left.MoveNext();
+                    }
+                    else
+                    {
+                        var cmp = Comparer.Compare(left.Current, right.Current);
+                        if (cmp <= 0)
+                        {
+                            result._values.Add(left.Current);
+                            leftHasNext = left.MoveNext();
+                        }
+                        else
+                        {
+                            result._values.Add(right.Current);
+                            rightHasNext = right.MoveNext();
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
 
         public void Clear()
--- ViewerTest/UI/Images/SortedListTest.cs
+++ ViewerTest/UI/Images/SortedListTest.cs
@@ -44,5 +44,31 @@ namespace ViewerTest.UI.Images
                 Assert.AreEqual(i, list[i]);
             }
         }
+
+        [TestMethod]
+        public void Merge_SortedLists()
+        {
+            var a = new SortedList<int>
+            {
+                1, 3, 5, 7
+            };
+            var b = new List<int>
+            {
+                2, 4, 6, 8, 9
+            };
+            var sorted = a.Merge(b);
+            CollectionAssert.AreEqual(new[] { 1, 3, 5, 7 }, a.ToArray());
+            CollectionAssert.AreEqual(new[] { 2, 4, 6, 8, 9 }, b.ToArray());
+            CollectionAssert.AreEqual(new[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, sorted.ToArray());
+        }
+
+        [TestMethod]
+        public void Merge_EmptySecondList()
+        {
+            var a = new SortedList<int>{ 1, 2, 3, 4, 5 };
+            var sorted = a.Merge(new int[]{});
+            CollectionAssert.AreEqual(new[] { 1, 2, 3, 4, 5 }, a.ToArray());
+            CollectionAssert.AreEqual(new[]{ 1, 2, 3, 4, 5 }, sorted.ToArray());
+        }
     }
 }

dba530fa9ee91d89eb9b6b00b2a712690b9e1179
trylock <lothar.gordon@gmail.com>
2018-07-29 20:30:59 +0200

    Editor: add synchronous method to open a file

--- Viewer/UI/Query/Editor.cs
+++ Viewer/UI/Query/Editor.cs
@@ -24,6 +24,14 @@ namespace Viewer.UI.Query
         Task<IDockContent> OpenAsync(string path);
 
         /// <summary>
+        /// Open given file in the editor.
+        /// If the file is opened already, the editor will just make its window visible to user.
+        /// </summary>
+        /// <param name="path">Path to a file</param>
+        /// <returns></returns>
+        IDockContent Open(string path);
+
+        /// <summary>
         /// Open new empty editor window
         /// </summary>
         /// <returns>Opened dock content</returns>
@@ -61,21 +69,46 @@ namespace Viewer.UI.Query
         public async Task<IDockContent> OpenAsync(string path)
         {
             // don't open a new window, if an window with this file is opened already
-            var window = _windows.Find(editor => 
-                StringComparer.CurrentCultureIgnoreCase.Compare(editor.Value.View.FullPath, path) == 0);
+            var window = FindWindow(path);
             if (window != null)
             {
-                window.Value.View.EnsureVisible();
-                return window.Value.View;
+                return window;
             }
 
             // otherwise load the file and show it in a new window
             try
             {
                 var data = await _fileSystem.ReadToEndAsync(path);
-                var editor = OpenWindow();
-                editor.SetContent(path, data);
-                return editor.View;
+                return OpenWindow(path, data);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                _dialogView.UnauthorizedAccess(path);
+            }
+            catch (SecurityException)
+            {
+                _dialogView.UnauthorizedAccess(path);
+            }
+            catch (FileNotFoundException)
+            {
+                _dialogView.FileNotFound(path);
+            }
+
+            return null;
+        }
+
+        public IDockContent Open(string path)
+        {
+            var window = FindWindow(path);
+            if (window != null)
+            {
+                return window;
+            }
+
+            try
+            {
+                var data = Encoding.UTF8.GetString(_fileSystem.ReadAllBytes(path));
+                return OpenWindow(path, data);
             }
             catch (UnauthorizedAccessException)
             {
@@ -104,6 +137,26 @@ namespace Viewer.UI.Query
             {
                 editor.Value.View.Close();
             }
+        }
+
+        private IDockContent FindWindow(string path)
+        {
+            var window = _windows.Find(editor =>
+                StringComparer.CurrentCultureIgnoreCase.Compare(editor.Value.View.FullPath, path) == 0);
+            if (window != null)
+            {
+                window.Value.View.EnsureVisible();
+                return window.Value.View;
+            }
+
+            return null;
+        }
+
+        private IDockContent OpenWindow(string path, string content)
+        {
+            var editor = OpenWindow();
+            editor.SetContent(path, content);
+            return editor.View;
         }
 
         private QueryPresenter OpenWindow()

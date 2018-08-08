1041971126d5b404f12cab6d8384fbbc7591c9b4
trylock <lothar.gordon@gmail.com>
2018-08-08 13:28:25 +0200

    Add query component
    This component does not have a UI window. It will just load
    query views on startup.

---
+++ Viewer.Query/QueryComponent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Viewer.IO;
+
+namespace Viewer.Query
+{
+    [Export(typeof(IComponent))]
+    public class QueryComponent : IComponent
+    {
+        private readonly IQueryViewRepository _queryViews;
+        private readonly IFileSystem _fileSystem;
+
+        [ImportingConstructor]
+        public QueryComponent(IQueryViewRepository queryViews, IFileSystem fileSystem)
+        {
+            _queryViews = queryViews;
+            _fileSystem = fileSystem;
+        }
+
+        public void OnStartup(IViewerApplication app)
+        {
+            LoadQueryViews("./views");
+        }
+
+        private void LoadQueryViews(string directoryPath)
+        {
+            try
+            {
+                var fullDirectoryPath = Path.GetFullPath(directoryPath);
+                foreach (var file in _fileSystem.EnumerateFiles(fullDirectoryPath, "*.vql"))
+                {
+                    LoadQueryView(file);
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+            }
+        }
+
+        private void LoadQueryView(string filePath)
+        {
+            try
+            {
+                var contents = _fileSystem.ReadAllText(filePath);
+                var name = Path.GetFileNameWithoutExtension(filePath);
+                _queryViews.Add(new QueryView(name, contents, filePath));
+            }
+            catch (DirectoryNotFoundException)
+            {
+            }
+            catch (FileNotFoundException)
+            {
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+            catch (SecurityException)
+            {
+            }
+            catch (IOException)
+            {
+            }
+        }
+    }
+}

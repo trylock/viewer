df9329af4510819a4725aa5efb16e4d6004525b0
trylock <lothar.gordon@gmail.com>
2018-08-10 21:47:01 +0200

    Add missing documentation of collections in the Core namespace

--- src/Viewer.Core/Collections/ConcurrentSortedSet.cs
+++ src/Viewer.Core/Collections/ConcurrentSortedSet.cs
@@ -9,20 +9,35 @@ using System.Threading.Tasks;
 namespace Viewer.Core.Collections
 {
     /// <summary>
-    /// Thread safe collection for sharing sorted collection among producer and consumer threads.
+    /// Thread safe sorted collection of items.
+    /// Producer threads can <see cref="Add"/> items to the collection and it will remain sorted by <see cref="Comparer"/>.
+    /// A consumer thread can take all items in the collection by calling <see cref="Consume"/>.
+    /// <see cref="Consume"/> will remove all items from the collections are return them to the caller in an order defined by <see cref="Comparer"/>.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">Type of an element in the collection</typeparam>
     public class ConcurrentSortedSet<T>
     {
+        /// <summary>
+        /// Comparer which defines order of items in this collection.
+        /// </summary>
         public IComparer<T> Comparer => _set.KeyComparer;
 
         private ImmutableSortedSet<T> _set;
 
+        /// <summary>
+        /// Create a new empty sorted collection with <paramref name="comparer"/>.
+        /// </summary>
+        /// <param name="comparer">Comparer used to sort this collection</param>
         public ConcurrentSortedSet(IComparer<T> comparer)
         {
             _set = ImmutableSortedSet<T>.Empty.WithComparer(comparer);
         }
 
+        /// <summary>
+        /// Add a new item to the collection. The collection will remain sorted by <see cref="Comparer"/>.
+        /// If <paramref name="item"/> has been in the collection already, it will *not* be added again.
+        /// </summary>
+        /// <param name="item">Item to add to his collection</param>
         public void Add(T item)
         {
             ImmutableSortedSet<T> newSet, oldSet;
@@ -33,6 +48,11 @@ namespace Viewer.Core.Collections
             } while (Interlocked.CompareExchange(ref _set, newSet, oldSet) != oldSet);
         }
 
+        /// <summary>
+        /// Take all items currently in the collection, remove them from the collection and return them.
+        /// Returend items will be sorted by using <see cref="Comparer"/>.
+        /// </summary>
+        /// <returns>Sorted snapshot of this collection.</returns>
         public IReadOnlyList<T> Consume()
         {
             var empty = ImmutableSortedSet<T>.Empty.WithComparer(Comparer);
--- src/Viewer.Core/Collections/SortedList.cs
+++ src/Viewer.Core/Collections/SortedList.cs
@@ -8,21 +8,40 @@ using System.Threading.Tasks;
 
 namespace Viewer.Core.Collections
 {
-    public class SortedList<T> : IEnumerable<T>
+    /// <inheritdoc cref="ICollection{T}" />
+    /// <inheritdoc cref="IReadOnlyList{T}" />
+    /// <summary>
+    /// Sorted list is a list of items sorted using <see cref="Comparer" />.
+    /// Random access to elements is a constant time operation.
+    /// Collection modification is an <c>O(n)</c> operation where <c>n</c> is <see cref="Count"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of an element in the collection</typeparam>
+    public class SortedList<T> : ICollection<T>, IReadOnlyList<T>
     {
         private readonly List<T> _values = new List<T>();
 
+        /// <summary>
+        /// Comparer used to sort items in the collection
+        /// </summary>
         public IComparer<T> Comparer { get; }
-
+        
         public int Count => _values.Count;
 
         public bool IsReadOnly => false;
         
+        /// <summary>
+        /// Create an empty sorted list with <paramref name="comparer"/>.
+        /// </summary>
+        /// <param name="comparer">Comparer of this list</param>
         public SortedList(IComparer<T> comparer)
         {
             Comparer = comparer;
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Create an empty sorted list with <see cref="P:System.Collections.Generic.Comparer`1.Default" /> as a comparer.
+        /// </summary>
         public SortedList() : this(Comparer<T>.Default)
         {
         }
@@ -37,12 +56,12 @@ namespace Viewer.Core.Collections
             return GetEnumerator();
         }
 
+        /// <inheritdoc />
         /// <summary>
-        /// Add a new item to the list.
-        /// The list will remain sorted.
-        /// Time complexity of this operation is linear with the size of this collection.
+        /// Add <paramref name="item"/> to the list. The list will remain sorted.
+        /// Time complexity of this operation is <c>O(n)</c> where <c>n</c> is <see cref="Count"/>.
         /// </summary>
-        /// <param name="item"></param>
+        /// <param name="item">Item added to the list</param>
         public void Add(T item)
         {
             _values.Add(item);
@@ -67,9 +86,9 @@ namespace Viewer.Core.Collections
 
         /// <summary>
         /// Merge 2 sorted collections to a new SortedList.
-        /// Neither collection will be modified.
-        /// Time complexity of this operation is linear with the combined size of the 2 collections.
-        /// This method does not check whether the second collection is sorted.
+        /// Neither collection will be modified. A new collection will be allocated, instead.
+        /// Time complexity of this operation is <c>O(n)</c> where <c>n</c> is <see cref="Count"/> + number of items in <paramref name="other"/>.
+        /// This method does not check whether <paramref name="other"/> is sorted.
         /// </summary>
         /// <param name="other">Other sorted collection which will be merged with this list.</param>
         /// <returns>A new sorted list which contains all elements from both lists.</returns>
@@ -113,16 +132,28 @@ namespace Viewer.Core.Collections
             return result;
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Remove all items in the collection.
+        /// Time complexity of this operation <c>O(n)</c> where <c>n</c> is <see cref="P:Viewer.Core.Collections.SortedList`1.Count" />
+        /// </summary>
         public void Clear()
         {
             _values.Clear();
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Check whether <paramref name="item" /> is in the collection.
+        /// Time complexity of this operation is <c>O(log(n))</c> where <c>n</c> is <see cref="P:Viewer.Core.Collections.SortedList`1.Count" />
+        /// </summary>
+        /// <param name="item">Searched item</param>
+        /// <returns>true iff <paramref name="item" /> is in this list</returns>
         public bool Contains(T item)
         {
             return IndexOf(item) >= 0;
         }
-
+        
         public void CopyTo(T[] array, int arrayIndex)
         {
             _values.CopyTo(array, arrayIndex);
@@ -133,11 +164,17 @@ namespace Viewer.Core.Collections
             return _values.Remove(item);
         }
 
+        /// <summary>
+        /// Find index of <paramref name="item"/> in the list.
+        /// Time complexity of this operation is <c>O(log(n))</c> where <c>n</c> is <see cref="Count"/>.
+        /// </summary>
+        /// <param name="item">Searched item</param>
+        /// <returns>Index of <paramref name="item"/> or -1 if <paramref name="item"/> is not in the collection</returns>
         public int IndexOf(T item)
         {
             return _values.BinarySearch(item, Comparer);
         }
-        
+
         public void RemoveAt(int index)
         {
             _values.RemoveAt(index);

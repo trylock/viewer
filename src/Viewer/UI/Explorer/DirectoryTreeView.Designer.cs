0ef3b12e00085c43fc02901d002222c807d2687c
trylock <lothar.gordon@gmail.com>
2018-08-15 22:57:38 +0200

    Fix: apply theme to explorer context menu

--- src/Viewer/UI/Explorer/DirectoryTreeView.Designer.cs
+++ src/Viewer/UI/Explorer/DirectoryTreeView.Designer.cs
@@ -162,6 +162,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(280, 295);
+            this.ContextMenuStrip = this.FileContextMenu;
             this.Controls.Add(this.TreeView);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);

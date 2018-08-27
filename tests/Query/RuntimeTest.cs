a3312e4e69e3a5a40ad426a0fa25c41650ade9bd
trylock <lothar.gordon@gmail.com>
2018-08-27 14:31:58 +0200

    Add missing test cases

--- tests/Query/RuntimeTest.cs
+++ tests/Query/RuntimeTest.cs
@@ -22,7 +22,9 @@ namespace ViewerTest.Query
         {
             _converter = new Mock<IValueConverter>();
             _testFunction = new Mock<IFunction>();
-            _testFunction.Setup(mock => mock.Name).Returns("test");
+            _testFunction
+                .Setup(mock => mock.Name)
+                .Returns("test");
             _runtime = new Runtime(_converter.Object, new IFunction[]{ _testFunction.Object });
         }
 
@@ -52,6 +54,39 @@ namespace ViewerTest.Query
         public void CallAndFind_UnknownFunction()
         {
             _runtime.FindAndCall("error", new IntValue(4), new StringValue("test"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QueryRuntimeException), "Unknown function test(Integer, Integer)")]
+        public void CallAndFind_DifferentNumberOfParameters()
+        {
+            _testFunction
+                .Setup(mock => mock.Arguments)
+                .Returns(new[] { TypeId.Integer });
+
+            _runtime.FindAndCall("test", new IntValue(1), new IntValue(2));
+        }
+
+        [TestMethod]
+        public void CallAndFind_IncompatibleArgumentTypes()
+        {
+            _testFunction
+                .Setup(mock => mock.Arguments)
+                .Returns(new[] { TypeId.Integer });
+            _testFunction
+                .Setup(mock => mock.Call(It.Is<ArgumentList>(args =>
+                    args.Count == 1 &&
+                    args.Get<IntValue>(0).IsNull)
+                ))
+                .Returns(new StringValue(null));
+
+            _converter
+                .Setup(mock => mock.ConvertTo(new StringValue("42"), TypeId.Integer))
+                .Returns(new IntValue(null));
+
+            var result = _runtime.FindAndCall("test", new StringValue("42"));
+            Assert.IsInstanceOfType(result, typeof(StringValue));
+            Assert.IsTrue(result.IsNull);
         }
     }
 }

358c373b52184150e9fb1f1321f2551a031485c0
trylock <lothar.gordon@gmail.com>
2018-09-06 23:48:23 +0200

    Feat: serialize/deserialize the presentation window
    Albeit only the window position and size at the moment.

--- src/Viewer/UI/Presentation/PresentationComponent.cs
+++ src/Viewer/UI/Presentation/PresentationComponent.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Viewer.Core;
+using Viewer.Core.UI;
 using Viewer.Data;
 using Viewer.Images;
 using Viewer.UI.Explorer;
@@ -35,26 +36,47 @@ namespace Viewer.UI.Presentation
 
         public override void OnStartup(IViewerApplication app)
         {
+            app.AddLayoutDeserializeCallback(Deserialize);
+        }
+
+        private IWindowView Deserialize(string persiststring)
+        {
+            if (persiststring == typeof(PresentationView).FullName)
+            {
+                return GetPresenter().View;
+            }
+
+            return null;
+        }
+
+        private PresentationPresenter GetPresenter()
+        {
+            if (_presenter != null)
+            {
+                return _presenter;
+            }
+
+            _presenter = new PresentationPresenter(new PresentationView(), _selection, _imageLoader, _dialogView);
+            _presenter.View.CloseView += (s, args) =>
+            {
+                _presenter.Dispose();
+                _presenter = null;
+            };
+            return _presenter;
         }
 
         private void ShowPresentation(IEnumerable<IEntity> entities, int index)
         {
             if (_presenter == null)
             {
-                _presenter = new PresentationPresenter(new PresentationView(), _selection, _imageLoader, _dialogView);
-                _presenter.View.CloseView += (s, args) =>
-                {
-                    _presenter.Dispose();
-                    _presenter = null;
-                };
+                _presenter = GetPresenter();
                 _presenter.View.Show(Application.Panel, DockState.Document);
-                _presenter.ShowEntity(entities, index);
             }
             else
             {
                 _presenter.View.EnsureVisible();
-                _presenter.ShowEntity(entities, index);
             }
+            _presenter.ShowEntity(entities, index);
         }
 
         public void Open(IEnumerable<IEntity> entities, int activeIndex)

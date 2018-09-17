965ec28e515b848094f828ff5f2d79ca623ce6ae
trylock <lothar.gordon@gmail.com>
2018-09-17 14:17:35 +0200

    Add rectangle extension method: EnsureInside

--- src/Viewer.Core/RectangleExtensions.cs
+++ src/Viewer.Core/RectangleExtensions.cs
@@ -18,5 +18,44 @@ namespace Viewer.Core
         {
             return rectangle.Size.Width * rectangle.Size.Height;
         }
+
+        /// <summary>
+        /// Transform <paramref name="rectangle"/> so that it is inside <paramref name="container"/>.
+        /// The size of <paramref name="rectangle"/> will only be modified if
+        /// <paramref name="container"/> is smaller than <paramref name="rectangle"/> is some axis.
+        /// </summary>
+        /// <param name="rectangle">Rectangle to transform</param>
+        /// <param name="container">
+        /// Container inside of which <paramref name="rectangle"/> should be placed.
+        /// </param>
+        /// <returns>Transformed rectangle</returns>
+        public static Rectangle EnsureInside(this Rectangle rectangle, Rectangle container)
+        {
+            // if container is smaller than rectangle, modify rectangle size now
+            rectangle.Size = new Size(
+                Math.Min(rectangle.Width, container.Width),
+                Math.Min(rectangle.Height, container.Height)
+            );
+
+            var deltaX = container.Right - rectangle.Right;
+            if (deltaX >= 0)
+            {
+                // if the difference is negative, the rectangle is in container in this dimension
+                deltaX = Math.Max(container.Left - rectangle.Left, 0);
+            }
+
+            var deltaY = container.Bottom - rectangle.Bottom;
+            if (deltaY >= 0)
+            {
+                // if the difference is negative, the rectangle is in container in this dimension
+                deltaY = Math.Max(container.Top - rectangle.Top, 0);
+            }
+
+            return new Rectangle(
+                rectangle.X + deltaX, 
+                rectangle.Y + deltaY,
+                rectangle.Width, 
+                rectangle.Height);
+        }
     }
 }
---
+++ tests/Core/RectangleExtensionsTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Viewer.Core;
+
+namespace ViewerTest.Core
+{
+    [TestClass]
+    public class RectangleExtensionsTest
+    {
+        [TestMethod]
+        public void EnsureInside_ContainerAndRectangleAreEmpty()
+        {
+            var result = Rectangle.Empty.EnsureInside(Rectangle.Empty);
+            Assert.AreEqual(Rectangle.Empty, result);
+        }
+
+        [TestMethod]
+        public void EnsureInside_ContainerIsEmpty()
+        {
+            var result = new Rectangle(0, 0, 10, 10).EnsureInside(Rectangle.Empty);
+            Assert.AreEqual(Rectangle.Empty, result);
+        }
+
+        [TestMethod]
+        public void EnsureInside_RectangleIsInTheMiddleOfContainer()
+        {
+            var result = new Rectangle(10, 10, 10, 10).EnsureInside(new Rectangle(0, 0, 30, 30));
+            Assert.AreEqual(new Rectangle(10, 10, 10, 10), result);
+        }
+
+        [TestMethod]
+        public void EnsureInside_RectangleOverflowsInTheXAxis()
+        {
+            var result = new Rectangle(10, 10, 10, 10).EnsureInside(new Rectangle(0, 0, 19, 30));
+            Assert.AreEqual(new Rectangle(9, 10, 10, 10), result);
+        }
+
+        [TestMethod]
+        public void EnsureInside_RectangleOverflowsInTheYAxis()
+        {
+            var result = new Rectangle(10, 10, 10, 10).EnsureInside(new Rectangle(0, 0, 30, 19));
+            Assert.AreEqual(new Rectangle(10, 9, 10, 10), result);
+        }
+
+        [TestMethod]
+        public void EnsureInside_RectangleOverflowsInTheXAndTheYAxis()
+        {
+            var result = new Rectangle(10, 10, 10, 10).EnsureInside(new Rectangle(0, 0, 19, 19));
+            Assert.AreEqual(new Rectangle(9, 9, 10, 10), result);
+        }
+    }
+}

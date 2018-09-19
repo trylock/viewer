719707a560e2f653a1262a87ac856d8f59dd728c
trylock <lothar.gordon@gmail.com>
2018-09-19 19:32:47 +0200

    Add generated files

---
+++ src/Viewer.QueryRuntime/ArithmeticOperators.cs
@@ -0,0 +1,193 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Viewer.Data;
+using Viewer.Query;
+
+namespace Viewer.QueryRuntime
+{
+                [Export(typeof(IFunction))]
+            public class IntValueAdditionFunction : IFunction
+            {
+                public string Name => "+";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Integer,
+                    TypeId.Integer
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<IntValue>(0);
+                    var rhs = context.Get<IntValue>(1);
+                    
+                    
+                    return new IntValue(lhs.Value + rhs.Value);
+                }
+            }
+                [Export(typeof(IFunction))]
+            public class IntValueSubtractionFunction : IFunction
+            {
+                public string Name => "-";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Integer,
+                    TypeId.Integer
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<IntValue>(0);
+                    var rhs = context.Get<IntValue>(1);
+                    
+                    
+                    return new IntValue(lhs.Value - rhs.Value);
+                }
+            }
+                [Export(typeof(IFunction))]
+            public class IntValueMultiplicationFunction : IFunction
+            {
+                public string Name => "*";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Integer,
+                    TypeId.Integer
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<IntValue>(0);
+                    var rhs = context.Get<IntValue>(1);
+                    
+                    
+                    return new IntValue(lhs.Value * rhs.Value);
+                }
+            }
+                [Export(typeof(IFunction))]
+            public class IntValueDivisionFunction : IFunction
+            {
+                public string Name => "/";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Integer,
+                    TypeId.Integer
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<IntValue>(0);
+                    var rhs = context.Get<IntValue>(1);
+                    
+                                        if (rhs.Value == 0)
+                    {
+                        return context.Error("Division by zero.");
+                    }
+                    
+                    return new IntValue(lhs.Value / rhs.Value);
+                }
+            }
+                [Export(typeof(IFunction))]
+            public class RealValueAdditionFunction : IFunction
+            {
+                public string Name => "+";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Real,
+                    TypeId.Real
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<RealValue>(0);
+                    var rhs = context.Get<RealValue>(1);
+                    
+                    
+                    return new RealValue(lhs.Value + rhs.Value);
+                }
+            }
+                [Export(typeof(IFunction))]
+            public class RealValueSubtractionFunction : IFunction
+            {
+                public string Name => "-";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Real,
+                    TypeId.Real
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<RealValue>(0);
+                    var rhs = context.Get<RealValue>(1);
+                    
+                    
+                    return new RealValue(lhs.Value - rhs.Value);
+                }
+            }
+                [Export(typeof(IFunction))]
+            public class RealValueMultiplicationFunction : IFunction
+            {
+                public string Name => "*";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Real,
+                    TypeId.Real
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<RealValue>(0);
+                    var rhs = context.Get<RealValue>(1);
+                    
+                    
+                    return new RealValue(lhs.Value * rhs.Value);
+                }
+            }
+                [Export(typeof(IFunction))]
+            public class RealValueDivisionFunction : IFunction
+            {
+                public string Name => "/";
+
+                public IReadOnlyList<TypeId> Arguments { get; } = new[]
+                {
+                    TypeId.Real,
+                    TypeId.Real
+                };
+
+                public BaseValue Call(IExecutionContext context)
+                {
+                    var lhs = context.Get<RealValue>(0);
+                    var rhs = context.Get<RealValue>(1);
+                    
+                    
+                    return new RealValue(lhs.Value / rhs.Value);
+                }
+            }
+    
+    [Export(typeof(IFunction))]
+    public class StringValueAdditionFunction : IFunction
+    {
+        public string Name => "+";
+
+        public IReadOnlyList<TypeId> Arguments { get; } = new[]
+        {
+            TypeId.String,
+            TypeId.String
+        };
+
+        public BaseValue Call(IExecutionContext arguments)
+        {
+            var lhs = arguments.Get<StringValue>(0);
+            var rhs = arguments.Get<StringValue>(1);
+            return new StringValue(lhs.Value + rhs.Value);
+        }
+    }
+}
---
+++ src/Viewer.QueryRuntime/LogicOperators.cs
@@ -0,0 +1,209 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Viewer.Data;
+using Viewer.Query;
+
+namespace Viewer.QueryRuntime
+{
+            [Export(typeof(IFunction))]
+        public class AndIntValueFunction : IFunction
+        {
+            public string Name => "and";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.Integer,
+                TypeId.Integer,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[0] : arguments[1];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class OrIntValueFunction : IFunction
+        {
+            public string Name => "or";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.Integer,
+                TypeId.Integer,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[1] : arguments[0];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class NotIntValueFunction : IFunction
+        {
+            public string Name => "not";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.Integer,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? new IntValue(1) : new IntValue(null);
+            }
+        }
+            [Export(typeof(IFunction))]
+        public class AndRealValueFunction : IFunction
+        {
+            public string Name => "and";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.Real,
+                TypeId.Real,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[0] : arguments[1];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class OrRealValueFunction : IFunction
+        {
+            public string Name => "or";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.Real,
+                TypeId.Real,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[1] : arguments[0];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class NotRealValueFunction : IFunction
+        {
+            public string Name => "not";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.Real,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? new IntValue(1) : new IntValue(null);
+            }
+        }
+            [Export(typeof(IFunction))]
+        public class AndStringValueFunction : IFunction
+        {
+            public string Name => "and";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.String,
+                TypeId.String,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[0] : arguments[1];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class OrStringValueFunction : IFunction
+        {
+            public string Name => "or";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.String,
+                TypeId.String,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[1] : arguments[0];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class NotStringValueFunction : IFunction
+        {
+            public string Name => "not";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.String,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? new IntValue(1) : new IntValue(null);
+            }
+        }
+            [Export(typeof(IFunction))]
+        public class AndDateTimeValueFunction : IFunction
+        {
+            public string Name => "and";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.DateTime,
+                TypeId.DateTime,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[0] : arguments[1];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class OrDateTimeValueFunction : IFunction
+        {
+            public string Name => "or";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.DateTime,
+                TypeId.DateTime,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? arguments[1] : arguments[0];
+            }
+        }
+
+        [Export(typeof(IFunction))]
+        public class NotDateTimeValueFunction : IFunction
+        {
+            public string Name => "not";
+
+            public IReadOnlyList<TypeId> Arguments { get; } = new[]
+            {
+                TypeId.DateTime,
+            };
+
+            public BaseValue Call(IExecutionContext arguments)
+            {
+                return arguments[0].IsNull ? new IntValue(1) : new IntValue(null);
+            }
+        }
+    }

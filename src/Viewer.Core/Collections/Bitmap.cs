cf2aa6ec8f9438d2b7a2ba80c28eb71cb26515b4
trylock <lothar.gordon@gmail.com>
2018-09-27 20:20:41 +0200

    Add Bitmap collection
    Bitmap is a compact collection of bits.

---
+++ src/Viewer.Core/Collections/Bitmap.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Core.Collections
+{
+    /// <summary>
+    /// Bitmap of arbitrary size.
+    /// </summary>
+    public class Bitmap : IReadOnlyList<bool>
+    {
+        private readonly List<ulong> _data;
+        private const int BlockSize = 64;
+
+        /// <summary>
+        /// Number of bits in the bitmap
+        /// </summary>
+        public int Count { get; }
+
+        /// <summary>
+        /// Create a new reseted (each bit is 0) bitmap with <paramref name="size"/> bits.
+        /// </summary>
+        /// <param name="size">Number of bits in the bitmap</param>
+        public Bitmap(int size)
+        {
+            if (size < 0)
+                throw new ArgumentOutOfRangeException(nameof(size));
+
+            Count = size;
+            _data = new List<ulong>();
+            var blockCount = MathUtils.RoundUpDiv(Count, BlockSize);
+            for (var i = 0; i < blockCount; ++i)
+            {
+                _data.Add(0);
+            }
+        }
+
+        public Bitmap(int size, bool state)
+        {
+            if (size < 0)
+                throw new ArgumentOutOfRangeException(nameof(size));
+
+            var value = state ? ulong.MaxValue : 0;
+            Count = size;
+            _data = new List<ulong>();
+            var blockCount = MathUtils.RoundUpDiv(Count, BlockSize);
+            for (var i = 0; i < blockCount; ++i)
+            {
+                _data.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// Copy <paramref name="other"/> bitmap.
+        /// </summary>
+        /// <param name="other">Bitmap to copy</param>
+        public Bitmap(Bitmap other)
+        {
+            Count = other.Count;
+            _data = new List<ulong>(other._data);
+        }
+
+        /// <summary>
+        /// Set <paramref name="index"/>th bit in the bitmap
+        /// </summary>
+        /// <param name="index">Index of a bit to set (to 1)</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <paramref name="index"/> is negative or greater than or equal to <see cref="Count"/>
+        /// </exception>
+        public void Set(int index)
+        {
+            if (index < 0 || index >= Count) 
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            SetUnchecked(index);
+        }
+        
+        private void SetUnchecked(int index)
+        {
+            var blockIndex = index / BlockSize;
+            var blockOffset = index % BlockSize;
+            var mask = 1UL << blockOffset;
+            _data[blockIndex] |= mask;
+        }
+
+        /// <summary>
+        /// Compute the bitwise and operation on 2 bitmaps. This operation will modify this bitmap.
+        /// <paramref name="bitmap"/> will remain unmodified.
+        /// </summary>
+        /// <param name="bitmap">Other bitmap</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <see cref="Count"/> are <paramref name="bitmap"/>.Count are different.
+        /// </exception>
+        public void And(Bitmap bitmap)
+        {
+            if (Count != bitmap.Count)
+                throw new ArgumentOutOfRangeException(nameof(bitmap));
+
+            for (var i = 0; i < _data.Count; ++i)
+            {
+                _data[i] &= bitmap._data[i];
+            }
+        }
+
+        /// <summary>
+        /// Compute the bitwise or operation on 2 bitmaps. This operation will modify this bitmap.
+        /// <paramref name="bitmap"/> will remain unmodified.
+        /// </summary>
+        /// <param name="bitmap">Other bitmap</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <see cref="Count"/> are <paramref name="bitmap"/>.Count are different.
+        /// </exception>
+        public void Or(Bitmap bitmap)
+        {
+            if (Count != bitmap.Count)
+                throw new ArgumentOutOfRangeException(nameof(bitmap));
+
+            for (var i = 0; i < _data.Count; ++i)
+            {
+                _data[i] |= bitmap._data[i];
+            }
+        }
+
+        /// <summary>
+        /// Compute the bitwise not operation. The operation will modify this bitmap.
+        /// </summary>
+        public void Not()
+        {
+            for (var i = 0; i < _data.Count; ++i)
+            {
+                _data[i] = ~_data[i];
+            }
+        }
+
+        /// <summary>
+        /// Get all bits in the bitmap
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<bool> GetEnumerator()
+        {
+            for (var i = 0; i < _data.Count; ++i)
+            {
+                var block = _data[i];
+                var blockSize = BlockSize;
+                if (i + 1 >= _data.Count)
+                {
+                    blockSize = Count % BlockSize;
+                }
+
+                for (var j = 0; j < blockSize; ++j)
+                {
+                    var mask = 1ul << j;
+                    yield return (block & mask) != 0;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Get a single bit from the bitmap
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public bool this[int index]
+        {
+            get
+            {
+                var blockIndex = index / BlockSize;
+                var blockOffset = index % BlockSize;
+                var blockMask = 1ul << blockOffset;
+                return (_data[blockIndex] & blockMask) != 0;
+            }
+        }
+    }
+}
---
+++ tests/Collections/BitmapTest.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Viewer.Core.Collections;
+
+namespace ViewerTest.Collections
+{
+    [TestClass]
+    public class BitmapTest
+    {
+        [TestMethod]
+        public void EmptyBitmap()
+        {
+            var bitmap = new Bitmap(0);
+            Assert.AreEqual(0, bitmap.Count);
+            CollectionAssert.AreEqual(new bool[0], bitmap.ToArray());
+        }
+
+        [TestMethod]
+        public void Set_SetBitsInSingleBlockBitmap()
+        {
+            var bitmap = new Bitmap(50);
+            Assert.AreEqual(50, bitmap.Count);
+
+            bitmap.Set(0);
+            bitmap.Set(49);
+            bitmap.Set(10);
+
+            var bits = new bool[bitmap.Count];
+            bits[0] = true;
+            bits[10] = true;
+            bits[49] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+        
+        [TestMethod]
+        public void Set_SetBitsInMultipleBlockBitmap()
+        {
+            var bitmap = new Bitmap(70);
+            Assert.AreEqual(70, bitmap.Count);
+
+            bitmap.Set(0);
+            bitmap.Set(63);
+            bitmap.Set(64);
+            bitmap.Set(69);
+
+            var bits = new bool[bitmap.Count];
+            bits[0] = true;
+            bits[63] = true;
+            bits[64] = true;
+            bits[69] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+
+        [TestMethod]
+        public void And_CombineEmptyBitmaps()
+        {
+            var bitmap = new Bitmap(0);
+            var other = new Bitmap(0);
+
+            Assert.AreEqual(0, bitmap.Count);
+            Assert.AreEqual(0, other.Count);
+
+            bitmap.And(other);
+
+            Assert.AreEqual(0, bitmap.Count);
+        }
+
+        [TestMethod]
+        public void And_CombineSingleBlockBitmaps()
+        {
+            var bitmap = new Bitmap(50);
+            var other = new Bitmap(50);
+
+            bitmap.Set(0);
+            bitmap.Set(10);
+            bitmap.Set(14);
+            bitmap.Set(22);
+            bitmap.Set(20);
+            bitmap.Set(42);
+            bitmap.Set(41);
+            bitmap.Set(43);
+
+            other.Set(42);
+            other.Set(21);
+
+            bitmap.And(other);
+
+            Assert.AreEqual(50, bitmap.Count);
+
+            var bits = new bool[bitmap.Count];
+            bits[42] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+
+        [TestMethod]
+        public void And_CombineMultipleBlockBitmaps()
+        {
+            var bitmap = new Bitmap(70);
+            var other = new Bitmap(70);
+
+            bitmap.Set(0);
+            bitmap.Set(10);
+            bitmap.Set(14);
+            bitmap.Set(22);
+            bitmap.Set(20);
+            bitmap.Set(42);
+            bitmap.Set(41);
+            bitmap.Set(43);
+            bitmap.Set(68);
+            bitmap.Set(66);
+            bitmap.Set(67);
+
+            other.Set(42);
+            other.Set(21);
+            other.Set(66);
+            other.Set(67);
+
+            bitmap.And(other);
+
+            Assert.AreEqual(70, bitmap.Count);
+
+            var bits = new bool[bitmap.Count];
+            bits[42] = true;
+            bits[66] = true;
+            bits[67] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+        
+        [TestMethod]
+        public void Or_CombineEmptyBitmaps()
+        {
+            var bitmap = new Bitmap(0);
+            var other = new Bitmap(0);
+
+            Assert.AreEqual(0, bitmap.Count);
+            Assert.AreEqual(0, other.Count);
+
+            bitmap.Or(other);
+
+            Assert.AreEqual(0, bitmap.Count);
+        }
+
+        [TestMethod]
+        public void Or_CombineSingleBlockBitmaps()
+        {
+            var bitmap = new Bitmap(50);
+            var other = new Bitmap(50);
+
+            bitmap.Set(0);
+            bitmap.Set(14);
+            bitmap.Set(49);
+
+            other.Set(13);
+            other.Set(24);
+            other.Set(49);
+            
+            bitmap.Or(other);
+
+            Assert.AreEqual(50, bitmap.Count);
+
+            var bits = new bool[bitmap.Count];
+            bits[0] = true;
+            bits[13] = true;
+            bits[14] = true;
+            bits[24] = true;
+            bits[49] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+
+        [TestMethod]
+        public void Or_CombineMultipleBlockBitmaps()
+        {
+            var bitmap = new Bitmap(70);
+            var other = new Bitmap(70);
+
+            bitmap.Set(0);
+            bitmap.Set(63);
+            bitmap.Set(64);
+            bitmap.Set(67);
+
+            other.Set(63);
+            other.Set(64);
+            other.Set(68);
+
+            bitmap.Or(other);
+
+            Assert.AreEqual(70, bitmap.Count);
+
+            var bits = new bool[bitmap.Count];
+            bits[0] = true;
+            bits[63] = true;
+            bits[64] = true;
+            bits[67] = true;
+            bits[68] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+
+        [TestMethod]
+        public void Not_EmptyBitmap()
+        {
+            var bitmap = new Bitmap(0);
+            Assert.AreEqual(0, bitmap.Count);
+            bitmap.Not();
+            Assert.AreEqual(0, bitmap.Count);
+            CollectionAssert.AreEqual(new bool[0], bitmap.ToArray());
+        }
+
+        [TestMethod]
+        public void Not_SingleBlockBitmap()
+        {
+            var bitmap = new Bitmap(50);
+
+            for (var i = 2; i < bitmap.Count - 2; ++i)
+            {
+                bitmap.Set(i);
+            }
+
+            bitmap.Not();
+
+            Assert.AreEqual(50, bitmap.Count);
+
+            var bits = new bool[bitmap.Count];
+            bits[0] = true;
+            bits[1] = true;
+            bits[48] = true;
+            bits[49] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+
+        [TestMethod]
+        public void Not_MultipleBlockBitmap()
+        {
+            var bitmap = new Bitmap(180);
+
+            for (var i = 2; i < bitmap.Count - 2; ++i)
+            {
+                if (i == 64)
+                {
+                    continue;
+                }
+                bitmap.Set(i);
+            }
+
+            bitmap.Not();
+
+            Assert.AreEqual(180, bitmap.Count);
+
+            var bits = new bool[bitmap.Count];
+            bits[0] = true;
+            bits[1] = true;
+            bits[64] = true;
+            bits[178] = true;
+            bits[179] = true;
+
+            CollectionAssert.AreEqual(bits, bitmap.ToArray());
+        }
+    }
+}

726a305972eef83b7cbc1925a65d61a15f471359
trylock <lothar.gordon@gmail.com>
2018-10-05 19:34:19 +0200

    Add SaveQueue
    Save queue adds all save requests to a queue so that they are ordered.
    This syncrhonizes them so that they don't compete for resources.

---
+++ src/Viewer/UI/Attributes/SaveQueue.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Primitives;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Viewer.Data;
+using Viewer.Properties;
+using Viewer.UI.Errors;
+using Viewer.UI.Tasks;
+
+namespace Viewer.UI.Attributes
+{
+    public interface ISaveQueue
+    {
+        /// <summary>
+        /// Save <paramref name="entities"/> to their file. If there is a save job running already,
+        /// saving <paramref name="entities"/> will be queued after it.
+        /// </summary>
+        /// <remarks>
+        /// This class is **not** thread safe and it should be called from the UI thread.
+        /// </remarks>
+        /// <param name="entities">Entities to save</param>
+        /// <returns>
+        /// Task finished once all <paramref name="entities"/> have been saved to their file or the
+        /// save task has been canceled. If it has been canceled, the task throws the
+        /// <see cref="OperationCanceledException"/>.
+        /// </returns>
+        Task SaveAsync(IReadOnlyList<IModifiedEntity> entities);
+
+        /// <summary>
+        /// Cancel all pending save operations. <see cref="SaveAsync"/> calls after the call to
+        /// this method will not be canceled.
+        /// </summary>
+        void Cancel();
+    }
+
+    internal class SaveState : IDisposable
+    {
+        /// <summary>
+        /// Cancellation token source used to cancel the save operation
+        /// </summary>
+        public CancellationTokenSource Cancellation { get; } = new CancellationTokenSource();
+
+        /// <summary>
+        /// Current progress controller
+        /// </summary>
+        public IProgressController ProgressController { get; }
+
+        public SaveState(ITaskLoader taskLoader)
+        {
+            ProgressController = taskLoader.CreateLoader(Resources.SavingChanges_Label, Cancellation);
+        }
+
+        public void Dispose()
+        {
+            Cancellation.Cancel();
+            ProgressController?.Close();
+            Cancellation?.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// Save job takes entities from the saving list of <see cref="IEntityManager"/> and saves them
+    /// to their file.
+    /// </summary>
+    [Export(typeof(ISaveQueue))]
+    internal class SaveQueue : ISaveQueue
+    {
+        private readonly ITaskLoader _taskLoader;
+
+        private int _saveTaskId = 0;
+        private Task _lastSaveTask = Task.CompletedTask;
+        private SaveState _state;
+
+        [ImportingConstructor]
+        public SaveQueue(ITaskLoader taskLoader)
+        {
+            _taskLoader = taskLoader;
+        }
+
+        public Task SaveAsync(IReadOnlyList<IModifiedEntity> entities)
+        {
+            if (entities.Count <= 0)
+            {
+                return Task.CompletedTask;
+            }
+
+            var id = ++_saveTaskId;
+            if (_state == null)
+            {
+                _state = new SaveState(_taskLoader);
+            }
+
+            _state.ProgressController.TotalTaskCount += entities.Count;
+            
+            // save all entities on background
+            _lastSaveTask = _lastSaveTask.ContinueWith((_, state) =>
+            {
+                var localEntities = (IReadOnlyList<IModifiedEntity>) state;
+                foreach (var entity in localEntities)
+                {
+                    if (_state.Cancellation.IsCancellationRequested)
+                    {
+                        entity.Return();
+                        continue;
+                    }
+
+                    _state.ProgressController.Report(new LoadingProgress(entity.Path));
+                    try
+                    {
+                        entity.Save();
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        entity.Return();
+                    }
+                    catch (IOException)
+                    {
+                        entity.Return();
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        entity.Return();
+                    }
+                    catch (SecurityException)
+                    {
+                        entity.Return();
+                    }
+                }
+            }, entities, CancellationToken.None, TaskContinuationOptions.LongRunning, TaskScheduler.Default);
+
+            // check if we should dispose current state (i.e., this has been the last save task)
+            _lastSaveTask = _lastSaveTask.ContinueWith(_ =>
+            {
+                if (id == _saveTaskId) // this has been the last save operation
+                {
+                    _state.Dispose();
+                    _state = null;
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            return _lastSaveTask;
+        }
+
+        public void Cancel()
+        {
+            _state?.Cancellation.Cancel();
+        }
+    }
+}
---
+++ tests/UI/Attributes/SaveQueueTest.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Viewer.Data;
+using Viewer.UI.Attributes;
+using Viewer.UI.Tasks;
+
+namespace ViewerTest.UI.Attributes
+{
+    [TestClass]
+    public class SaveQueueTest
+    {
+        private Mock<ITaskLoader> _taskLoader;
+        private Mock<IProgressController> _progressController;
+        private SaveQueue _queue;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _progressController = new Mock<IProgressController>();
+            _taskLoader = new Mock<ITaskLoader>();
+            _taskLoader
+                .Setup(mock => mock.CreateLoader(It.IsAny<string>(), It.IsAny<CancellationTokenSource>()))
+                .Returns(_progressController.Object);
+
+            _queue = new SaveQueue(_taskLoader.Object);
+            
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+        }
+
+        [TestMethod]
+        public async Task SaveAsync_NoItems()
+        {
+            await _queue.SaveAsync(new IModifiedEntity[] {});
+            
+            _taskLoader.Verify(mock => mock.CreateLoader(It.IsAny<string>(), It.IsAny<CancellationTokenSource>()), Times.Never);
+        }
+
+        [TestMethod]
+        public async Task SaveAsync_OneItem()
+        {
+            var item = new Mock<IModifiedEntity>();
+            await _queue.SaveAsync(new[] { item.Object });
+
+            item.Verify(mock => mock.Save(), Times.Once);
+            item.Verify(mock => mock.Return(), Times.Never);
+            item.Verify(mock => mock.Revert(), Times.Never);
+
+            _progressController.VerifySet(mock => mock.TotalTaskCount = 1, Times.Once);
+            _progressController.Verify(mock => mock.Close(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task SaveAsync_MultipleBlocks()
+        {
+            var mocks = new Mock<IModifiedEntity>[100];
+            for (var i = 0; i < mocks.Length; ++i)
+            {
+                mocks[i] = new Mock<IModifiedEntity>();
+            }
+
+            // save all blocks
+            var tasks = new Task[mocks.Length];
+            for (var i = 0; i < tasks.Length; ++i)
+            {
+                var mock = mocks[i];
+                tasks[i] = _queue.SaveAsync(new []{ mock.Object });
+            }
+
+            await Task.WhenAll(tasks);
+
+            foreach (var mock in mocks)
+            {
+                mock.Verify(it => it.Save(), Times.Once);
+                mock.Verify(it => it.Return(), Times.Never);
+                mock.Verify(it => it.Revert(), Times.Never);
+            }
+            
+            _taskLoader.Verify(mock => mock.CreateLoader(It.IsAny<string>(), It.IsAny<CancellationTokenSource>()), Times.Once);
+            // the progress controller mock is setup up so that is returns 0, thus each save operation
+            // should set it to 1
+            _progressController.VerifySet(mock => mock.TotalTaskCount = 1, Times.Exactly(mocks.Length));
+            _progressController.Verify(mock => mock.Close(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task SaveAsync_ReturnItemsToTheModifiedList()
+        {
+            var mocks = new Mock<IModifiedEntity>[10];
+            for (var i = 0; i < mocks.Length; ++i)
+            {
+                mocks[i] = new Mock<IModifiedEntity>();
+            }
+
+            // the first mock will cancel the execution
+            mocks[0]
+                .Setup(mock => mock.Save())
+                .Callback(() => _queue.Cancel());
+
+            await _queue.SaveAsync(mocks.Select(item => item.Object).ToList());
+
+            mocks[0].Verify(mock => mock.Save(), Times.Once);
+            mocks[0].Verify(mock => mock.Return(), Times.Never);
+            mocks[0].Verify(mock => mock.Revert(), Times.Never);
+            for (var i = 1; i < mocks.Length; ++i)
+            {
+                var mock = mocks[i];
+                mock.Verify(it => it.Save(), Times.Never);
+                mock.Verify(it => it.Return(), Times.Once);
+                mock.Verify(it => it.Revert(), Times.Never);
+            }
+        }
+
+        [TestMethod]
+        public async Task SaveAsync_ReturnEntityToTheModifiedListInCaseOfAnException()
+        {
+            var mocks = new Mock<IModifiedEntity>[10];
+            for (var i = 0; i < mocks.Length; ++i)
+            {
+                mocks[i] = new Mock<IModifiedEntity>();
+            }
+
+            mocks[0]
+                .Setup(mock => mock.Save())
+                .Throws(new FileNotFoundException());
+            mocks[1]
+                .Setup(mock => mock.Save())
+                .Throws(new IOException());
+            mocks[2]
+                .Setup(mock => mock.Save())
+                .Throws(new UnauthorizedAccessException());
+            mocks[3]
+                .Setup(mock => mock.Save())
+                .Throws(new SecurityException());
+
+            await _queue.SaveAsync(mocks.Select(item => item.Object).ToList());
+
+            for (var i = 0; i < 4; ++i)
+            {
+                mocks[i].Verify(mock => mock.Save(), Times.Once);
+                mocks[i].Verify(mock => mock.Return(), Times.Once);
+                mocks[i].Verify(mock => mock.Revert(), Times.Never);
+            }
+
+            for (var i = 4; i < mocks.Length; ++i)
+            {
+                mocks[i].Verify(mock => mock.Save(), Times.Once);
+                mocks[i].Verify(mock => mock.Return(), Times.Never);
+                mocks[i].Verify(mock => mock.Revert(), Times.Never);
+            }
+        }
+    }
+}

8e4dc1318a6190365894253c366f1f360de8a63f
trylock <lothar.gordon@gmail.com>
2018-10-07 19:21:43 +0200

    Add CaretTokenSource
    Caret token source lexes the input until a given position is reached.
    A new caret token is inserted at the location. If the position is inside
    of another token, this token won't be returned. Instread, it will be
    set as a parent token in the returned CaretToken.

---
+++ src/Viewer.Query/Suggestions/CaretToken.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace Viewer.Query.Suggestions
+{
+    /// <summary>
+    /// Token which represents caret in the input. 
+    /// </summary>
+    /// <remarks>
+    /// This token is not defined or used in the lexer. Instead, it is added programmatically
+    /// during query suggestion computation.
+    /// </remarks>
+    public class CaretToken : CommonToken
+    {
+        /// <summary>
+        /// Type of the caret token
+        /// </summary>
+        public const int TokenType = 9000;
+
+        /// <summary>
+        /// Token in which the caret is. This can be null if the caret is not in any token.
+        /// </summary>
+        public IToken ParentToken { get; }
+
+        /// <summary>
+        /// Index of caret within <see cref="ParentToken"/> or -1 if
+        /// <see cref="ParentToken"/> is null.
+        /// </summary>
+        public int ParentOffset { get; }
+
+        /// <summary>
+        /// Get prefix of the container token which ends at the caret or null if this caret does
+        /// not have a parent token.
+        /// </summary>
+        public string ParentPrefix => ParentToken?.Text?.Substring(0, ParentOffset);
+
+        /// <summary>
+        /// Create a caret token without container token
+        /// </summary>
+        public CaretToken(
+            ITokenSource source,
+            ICharStream stream,
+            int position) 
+            : base(
+                new Antlr4.Runtime.Sharpen.Tuple<ITokenSource, ICharStream>(source, stream), 
+                TokenType, 
+                Lexer.DefaultTokenChannel,
+                position, 
+                position)
+        {
+            ParentToken = null;
+            ParentOffset = -1;
+        }
+
+        /// <summary>
+        /// Create caret token with container token
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="stream"></param>
+        /// <param name="position">Index of the caret in the input</param>
+        /// <param name="parentToken">Container in which the caret is</param>
+        public CaretToken(
+            ITokenSource source, 
+            ICharStream stream,
+            int position,
+            IToken parentToken) 
+            : base(
+                new Antlr4.Runtime.Sharpen.Tuple<ITokenSource, ICharStream>(source, stream), 
+                TokenType, 
+                Lexer.DefaultTokenChannel,
+                position, 
+                position)
+        {
+            if (parentToken == null)
+                throw new ArgumentNullException(nameof(parentToken));
+            if (position < parentToken.StartIndex)
+                throw new ArgumentOutOfRangeException(nameof(position));
+
+            ParentToken = parentToken;
+            ParentOffset = position - parentToken.StartIndex;
+        }
+    }
+}
---
+++ src/Viewer.Query/Suggestions/CaretTokenSource.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace Viewer.Query.Suggestions
+{
+    /// <summary>
+    /// Caret token source returns all tokens up to a certain point (the caret position). After
+    /// this point, it returns <see cref="CaretToken"/> instead.
+    /// </summary>
+    internal class CaretTokenSource : ITokenSource
+    {
+        private readonly Lexer _lexer;
+        private readonly int _caretPosition;
+
+        public CaretTokenSource(Lexer lexer, int caretPosition)
+        {
+            _lexer = lexer;
+            _caretPosition = caretPosition;
+        }
+
+        private bool _caretTokenEmitted;
+
+        public IToken NextToken()
+        {
+            var token = _lexer.NextToken();
+
+            Trace.Assert(token.StartIndex >= 0);
+
+            // emit caret token
+            if (!_caretTokenEmitted)
+            {
+                if (token.Type != Lexer.Eof &&
+                    token.StartIndex <= _caretPosition &&
+                    token.StopIndex + 1 >= _caretPosition)
+                {
+                    _caretTokenEmitted = true;
+                    return new CaretToken(_lexer, InputStream, _caretPosition, token);
+                }
+                else if (token.Type == Lexer.Eof || token.StartIndex > _caretPosition)
+                {
+                    _caretTokenEmitted = true;
+                    return new CaretToken(_lexer, InputStream, _caretPosition);
+                }
+            }
+
+            // emit EOF token after the caret
+            if (_caretTokenEmitted)
+            {
+                return new CommonToken(Lexer.Eof);
+            }
+            return token;
+        }
+
+        public int Line => _lexer.Line;
+        public int Column => _lexer.Column;
+        public ICharStream InputStream => ((ITokenSource)_lexer).InputStream;
+        public string SourceName => _lexer.SourceName;
+        public ITokenFactory TokenFactory
+        {
+            get => _lexer.TokenFactory;
+            set => _lexer.TokenFactory = value;
+        }
+    }
+
+}

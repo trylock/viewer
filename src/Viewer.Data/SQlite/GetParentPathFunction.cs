1280329ad16c835fda6ef667c258bacd87cec5c6
trylock <lothar.gordon@gmail.com>
2018-10-30 21:22:40 +0100

    Add GetParentPath SQLite function

---
+++ src/Viewer.Data/SQlite/GetParentPathFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Viewer.IO;
+
+namespace Viewer.Data.SQLite
+{
+    [SQLiteFunction(Name = "getParentPath", Arguments = 1, FuncType = FunctionType.Scalar)]
+    public class GetParentPathFunction : SQLiteFunction
+    {
+        public override object Invoke(object[] args)
+        {
+            var path = args[0].ToString();
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return null;
+            }
+
+            var pattern = new PathPattern(path);
+            var parent = pattern.GetParent();
+            return parent?.Text;
+        }
+    }
+}

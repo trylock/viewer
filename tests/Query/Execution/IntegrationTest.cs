fb44751e3a9a6ae9004b028c84f47bd5326c5ceb
trylock <lothar.gordon@gmail.com>
2018-11-09 12:37:44 +0100

    Test the new query evaluator

---
+++ tests/Query/Execution/IntegrationTest.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Viewer.Data;
+using Viewer.Data.Storage;
+using Viewer.IO;
+using Viewer.Query;
+using Viewer.Query.Execution;
+
+namespace ViewerTest.Query.Execution
+{ 
+    [Export(typeof(IQueryErrorListener))]
+    public class ErrorListener : IQueryErrorListener
+    {
+        public void BeforeCompilation()
+        {
+        }
+
+        public void OnCompilerError(int line, int column, string errorMessage)
+        {
+        }
+
+        public void OnRuntimeError(int line, int column, string errorMessage)
+        {
+        }
+
+        public void AfterCompilation()
+        {
+        }
+    }
+    
+    [Export(typeof(IStorageConfiguration))]
+    public class Configuration : IStorageConfiguration
+    {
+        public TimeSpan CacheLifespan => TimeSpan.FromDays(7);
+        public int CacheMaxFileCount => int.MaxValue;
+    }
+
+    [TestClass]
+    public class IntegrationTest
+    {
+        private IExecutableQuery Compile(string text)
+        {
+            var catalog = new AggregateCatalog(
+                new AssemblyCatalog(Assembly.GetAssembly(typeof(Viewer.Data.IEntity))),
+                new AssemblyCatalog(Assembly.GetAssembly(typeof(Viewer.Query.IRuntime))),
+                new AssemblyCatalog(Assembly.GetAssembly(typeof(Viewer.QueryRuntime.IntValueAdditionFunction))),
+                new AssemblyCatalog(Assembly.GetAssembly(typeof(Viewer.IO.IFileSystem))),
+                new TypeCatalog(typeof(ErrorListener)),
+                new TypeCatalog(typeof(Configuration)));
+
+            using (var container = new CompositionContainer(catalog))
+            {
+                var compiler = container.GetExportedValue<IQueryCompiler>();
+                return compiler.Compile(text);
+            }
+        }
+
+        private class ResultSet : IReadOnlyList<IEntity>
+        {
+            private readonly List<IEntity> _entities = new List<IEntity>();
+            
+            public ResultSet(IEnumerable<IEntity> entities)
+            {
+                _entities.AddRange(entities);
+            }
+            
+            public string[] GetPaths()
+            {
+                return _entities.Select(item => item.Path).ToArray();
+            }
+
+            public IEnumerator<IEntity> GetEnumerator()
+            {
+                return _entities.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            public int Count => _entities.Count;
+
+            public IEntity this[int index] => _entities[index];
+
+            public bool ContainsFileAt(string path)
+            {
+                path = PathUtils.NormalizePath(path);
+                var result = _entities.Find(entity =>
+                    string.Equals(entity.Path, path, StringComparison.CurrentCultureIgnoreCase));
+                return result != null;
+            }
+        }
+
+        private ResultSet Execute(string query)
+        {
+            var compiledQuery = Compile(query);
+            return new ResultSet(compiledQuery.Execute(new ExecutionOptions()));
+        }
+
+        private const string BaseDir = "../../ExecutionTestData";
+
+        [TestMethod]
+        public void Select_NonExistentFolder()
+        {
+            var result = Execute("select \"" + BaseDir + "/**/*error*\"");
+            
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Select_Subfolder()
+        {
+            var result = Execute("select \"" + BaseDir + "/**/c\"");
+            
+            Assert.AreEqual(4, result.Count);
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item13.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item14.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item15.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item16.jpg"));
+        }
+
+        [TestMethod]
+        public void Select_Subtree()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\"");
+            
+            Assert.AreEqual(19, result.Count);
+            // files
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item1.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item2.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item3.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item4.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item5.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item6.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item7.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item8.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item9.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item10.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item11.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item12.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item13.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item14.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item15.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item16.jpg"));
+            // folders
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c"));
+        }
+
+        [TestMethod]
+        public void Where_AttributeNamePredicate()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where attr1");
+            
+            Assert.AreEqual(6, result.Count);
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item1.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item3.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item4.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item5.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item13.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item16.jpg"));
+        }
+
+        [TestMethod]
+        public void Where_NamePredicateInDNF()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where (attr1 and not attr2) or (attr2 and not attr1)");
+            
+            Assert.AreEqual(11, result.Count);
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item1.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item2.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item4.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item5.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item7.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item8.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item9.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item11.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item12.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item13.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item16.jpg"));
+        }
+
+        [TestMethod]
+        public void Where_NamePredicateInCNF()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where (attr1 or attr2) and (not attr1 or not attr2)");
+            
+            Assert.AreEqual(11, result.Count);
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item1.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item2.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item4.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item5.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item7.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item8.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item9.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item11.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item12.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item13.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item16.jpg"));
+        }
+
+        [TestMethod]
+        public void Where_GreaterThanPredicate()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where attr2 > 24");
+            
+            Assert.AreEqual(4, result.Count);
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item2.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item7.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item8.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item9.jpg"));
+        }
+
+        [TestMethod]
+        public void Where_LessThanPredicate()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where attr2 < 24");
+            
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Where_EqualsPredicate()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where attr2 = 42");
+            
+            Assert.AreEqual(4, result.Count);
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item2.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item7.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item8.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item9.jpg"));
+        }
+
+        [TestMethod]
+        public void Where_NotEqual()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where attr1 != \"value\"");
+            
+            Assert.AreEqual(1, result.Count);
+            
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item16.jpg"));
+        }
+
+        [TestMethod]
+        public void Where_NegatedEquals()
+        {
+            var result = Execute("select \"" + BaseDir + "/**\" where not (attr1 = \"value\")");
+            
+            Assert.AreEqual(14, result.Count);
+
+            // files
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/item2.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a/item6.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item7.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item8.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item9.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item10.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item11.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/item12.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item14.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item15.jpg"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c/item16.jpg"));
+            // folders
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/a"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b"));
+            Assert.IsTrue(result.ContainsFileAt(BaseDir + "/b/c"));
+        }
+    }
+}
---
+++ tests/Query/Execution/SimpleQueryTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Viewer.Data;
+using Viewer.Data.Storage;
+using Viewer.IO;
+using Viewer.Query;
+using Viewer.Query.Execution;
+using Viewer.Query.Expressions;
+using Viewer.Query.Search;
+using Attribute = Viewer.Data.Attribute;
+
+namespace ViewerTest.Query.Execution
+{
+    [TestClass]
+    public class SimpleQueryTest
+    {
+        private Mock<IEntityManager> _entityManager;
+        private Mock<IFileSystem> _fileSystem;
+        private Mock<IFileFinder> _fileFinder;
+        private Mock<IRuntime> _runtime;
+        private Mock<IPriorityComparerFactory> _priorityComparerFactory;
+        private Mock<IReadableAttributeStorage> _reader;
+        
+        private SimpleQuery Create(string pattern, ValueExpression predicate, IComparer<IEntity> comparer)
+        {
+            return new SimpleQuery(
+                _entityManager.Object,
+                _fileSystem.Object,
+                _runtime.Object,
+                _priorityComparerFactory.Object,
+                pattern,
+                predicate, 
+                comparer, ""
+            );
+        }
+
+        private ValueExpression CreatePredicate(Func<IEntity, bool> func)
+        {
+            var predicate = new Mock<ValueExpression>();
+            predicate
+                .Setup(mock => mock.CompilePredicate(It.IsAny<IRuntime>()))
+                .Returns(func);
+            return predicate.Object;
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _entityManager = new Mock<IEntityManager>();
+            _fileSystem = new Mock<IFileSystem>();
+            _runtime = new Mock<IRuntime>();
+            _priorityComparerFactory = new Mock<IPriorityComparerFactory>();
+            _reader = new Mock<IReadableAttributeStorage>();
+            _fileFinder = new Mock<IFileFinder>();
+
+            _fileSystem
+                .Setup(mock => mock.CreateFileFinder(It.IsAny<string>()))
+                .Returns(_fileFinder.Object);
+            _fileFinder.Setup(mock => mock.Pattern).Returns(new PathPattern("test"));
+            _entityManager.Setup(mock => mock.CreateReader()).Returns(_reader.Object);
+        }
+
+        [TestMethod]
+        public void Execute_NoMatchingDirectories()
+        {
+            var predicate = CreatePredicate(entity => true);
+            var query = Create("test", predicate, EntityComparer.Default);
+
+            _fileFinder
+                .Setup(mock => mock.GetDirectories(It.IsAny<IComparer<string>>()))
+                .Returns(new string[] { });
+
+            var result = query.Execute(new ExecutionOptions()).ToList();
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Execute_QueryWithPredicate()
+        {
+            var predicate = CreatePredicate(entity =>
+            {
+                return entity.GetAttribute("a") == null;
+            });
+            var query = Create("test", predicate, EntityComparer.Default);
+            
+            _fileFinder
+                .Setup(mock => mock.GetDirectories(It.IsAny<IComparer<string>>()))
+                .Returns(new[]
+                {
+                    "a/b", "a/c"
+                });
+
+            _fileSystem
+                .Setup(mock => mock.EnumerateFiles("a/b"))
+                .Returns(new[] {"a/b/test.jpeg"});
+            _fileSystem
+                .Setup(mock => mock.EnumerateDirectories("a/b"))
+                .Returns(new string[] {});
+            _fileSystem
+                .Setup(mock => mock.EnumerateFiles("a/c"))
+                .Returns(new string[] { "a/c/test2.jpg" });
+            _fileSystem
+                .Setup(mock => mock.EnumerateDirectories("a/c"))
+                .Returns(new string[] { "a/c/dir" });
+
+            var results = new IEntity[]
+            {
+                new FileEntity("a/b/test.jpeg"),
+                new FileEntity("a/c/test2.jpg")
+                    .SetAttribute(new Attribute("a", new IntValue(1), AttributeSource.Custom)),
+                new DirectoryEntity("a/c/dir"), 
+            };
+            _reader
+                .Setup(mock => mock.Load("a/b/test.jpeg"))
+                .Returns(results[0]);
+            _reader
+                .Setup(mock => mock.Load("a/c/test2.jpg"))
+                .Returns(results[1]);
+            _reader
+                .Setup(mock => mock.Load("a/c/dir"))
+                .Returns(results[2]);
+
+            // execute the query
+            var result = query.Execute(new ExecutionOptions()).ToList();
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(result[0], results[0]);
+            Assert.IsInstanceOfType(result[1], typeof(DirectoryEntity));
+            Assert.AreEqual(result[1].Path, results[2].Path);
+        }
+    }
+}

8073cb160630133c5d29f628e82f31c5ed5afe75
trylock <lothar.gordon@gmail.com>
2018-12-07 22:45:40 +0100

    Move list extensions to a custom class

---
+++ src/Viewer.Core/Collections/ListExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Core.Collections
+{
+    /// <summary>
+    /// Extension of a IList , IReadOnlyList types
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Find the lower bound of <paramref name="value"/> in <paramref name="list"/>.
+        /// </summary>
+        /// <remarks>
+        /// Lower bound is the first value which is greater than <paramref name="value"/> according
+        /// to <paramref name="comparer"/>. 
+        /// </remarks>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        /// <param name="comparer"></param>
+        /// <returns>
+        /// The first index which is greater than <paramref name="value"/> or length of the
+        /// <paramref name="list"/> if all values are at most <paramref name="value"/>.
+        /// </returns>
+        public static int LowerBound<T>(this IList<T> list, T value, IComparer<T> comparer)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+            if (comparer == null)
+                throw new ArgumentNullException(nameof(comparer));
+
+            var begin = 0;
+            var end = list.Count;
+            while (begin < end)
+            {
+                var index = (begin + end) / 2;
+                if (comparer.Compare(list[index], value) < 0)
+                {
+                    begin = index + 1;
+                }
+                else
+                {
+                    end = index;
+                }
+            }
+
+            return end;
+        }
+
+        /// <summary>
+        /// Same as <see cref="LowerBound{T}(IList{T}, T, IComparer{T})"/> but it uses the
+        /// default comparer for <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int LowerBound<T>(this IList<T> list, T value)
+        {
+            return list.LowerBound(value, Comparer<T>.Default);
+        }
+    }
+}
---
+++ tests/Collections/ListExtensionsTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Viewer.Core.Collections;
+
+namespace ViewerTest.Collections
+{
+    [TestClass]
+    public class ListExtensionsTest
+    {
+        [TestMethod]
+        public void LowerBound_EmptyList()
+        {
+            var list = new List<int>();
+            var bound = list.LowerBound(1);
+            Assert.AreEqual(0, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsBeforeTheFirstElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(0);
+            Assert.AreEqual(0, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsTheFirstElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(1);
+            Assert.AreEqual(0, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsAfterTheFirstElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(2);
+            Assert.AreEqual(1, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsBeforeTheLastElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(4);
+            Assert.AreEqual(2, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsTheLastElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(5);
+            Assert.AreEqual(2, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsAfterTheLastElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(6);
+            Assert.AreEqual(3, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsTheMiddleElement()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            var bound = list.LowerBound(4);
+            Assert.AreEqual(3, bound);
+        }
+    }
+}

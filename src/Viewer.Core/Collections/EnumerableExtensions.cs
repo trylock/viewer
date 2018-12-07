16b5beb7a1eb9915b7a1c31e828f70b6fc67919c
trylock <lothar.gordon@gmail.com>
2018-12-07 21:49:14 +0100

    Add list LowerBound extension method

--- src/Viewer.Core/Collections/EnumerableExtensions.cs
+++ src/Viewer.Core/Collections/EnumerableExtensions.cs
@@ -72,6 +72,51 @@ namespace Viewer.Core.Collections
         }
 
         /// <summary>
+        /// Find the lower bound of <paramref name="value"/> in <paramref name="list"/>.
+        /// </summary>
+        /// <remarks>
+        /// Lower bound is the first value which is greater than <paramref name="value"/> according
+        /// to <paramref name="comparer"/>. 
+        /// </remarks>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        /// <param name="comparer"></param>
+        /// <returns>
+        /// The first index which is greater than <paramref name="value"/> or length of the
+        /// <paramref name="list"/> if all values are at most <paramref name="value"/>.
+        /// </returns>
+        public static int LowerBound<T>(this IList<T> list, T value, IComparer<T> comparer)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+            if (comparer == null)
+                throw new ArgumentNullException(nameof(comparer));
+
+            var begin = 0;
+            var end = list.Count;
+            while (begin < end)
+            {
+                var index = (begin + end) / 2;
+                if (comparer.Compare(list[index], value) < 0)
+                {
+                    begin = index + 1;
+                }
+                else
+                {
+                    end = index;
+                }
+            }
+
+            return end;
+        }
+
+        public static int LowerBound<T>(this IList<T> list, T value)
+        {
+            return list.LowerBound(value, Comparer<T>.Default);
+        }
+
+        /// <summary>
         /// Same as <see cref="ToHashSet{T}(System.Collections.Generic.IEnumerable{T})"/> but
         /// it uses <see cref="EqualityComparer{T}.Default"/> comparer.
         /// </summary>
--- tests/Collections/EnumerableExtensionsTest.cs
+++ tests/Collections/EnumerableExtensionsTest.cs
@@ -65,5 +65,69 @@ namespace ViewerTest.Collections
             Assert.AreNotEqual(second, merged);
             CollectionAssert.AreEqual(new[] { 1, 2, 2, 5, 9 }, merged);
         }
+
+        [TestMethod]
+        public void LowerBound_EmptyList()
+        {
+            var list = new List<int>();
+            var bound = list.LowerBound(1);
+            Assert.AreEqual(0, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsBeforeTheFirstElement()
+        {
+            var list = new List<int>{ 1, 3, 5 };
+            var bound = list.LowerBound(0);
+            Assert.AreEqual(0, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsTheFirstElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(1);
+            Assert.AreEqual(0, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsAfterTheFirstElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(2);
+            Assert.AreEqual(1, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsBeforeTheLastElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(4);
+            Assert.AreEqual(2, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsTheLastElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(5);
+            Assert.AreEqual(2, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsAfterTheLastElement()
+        {
+            var list = new List<int> { 1, 3, 5 };
+            var bound = list.LowerBound(6);
+            Assert.AreEqual(3, bound);
+        }
+
+        [TestMethod]
+        public void LowerBound_ValueIsTheMiddleElement()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            var bound = list.LowerBound(4);
+            Assert.AreEqual(3, bound);
+        }
     }
 }
